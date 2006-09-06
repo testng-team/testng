f3d1d331cec5889e7ce12c7208f7be800dcd12e2
the.mindstorm <the.mindstorm@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-09-06 18:55:05 +0000

    fixed JDK5 API issue

--- test-14/src/test/thread/SequentialSampleTest.java
+++ test-14/src/test/thread/SequentialSampleTest.java
@@ -25,7 +25,7 @@ public class SequentialSampleTest {
    * @testng.test
    */
   public void f1() {
-    Long id = new Long(Thread.currentThread().getId());
+    Long id = new Long(Thread.currentThread().hashCode());
     ppp("ID:" + id);
     m_threads.put(id, id);
   }
@@ -34,7 +34,7 @@ public class SequentialSampleTest {
    * @testng.test
    */
   public void f2() {
-    Long id = new Long(Thread.currentThread().getId());
+    Long id = new Long(Thread.currentThread().hashCode());
     ppp("ID:" + id);
     m_threads.put(id, id);
   }
@@ -43,7 +43,7 @@ public class SequentialSampleTest {
    * @testng.test
    */
   public void f3() {
-    Long id = new Long(Thread.currentThread().getId());
+    Long id = new Long(Thread.currentThread().hashCode());
     ppp("ID:" + id);
     m_threads.put(id, id);
   }
