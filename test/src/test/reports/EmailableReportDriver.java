c31cdfda08dd00152fab209c9e7f06d1aeda35e6
mendelson.paul <mendelson.paul@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-09-23 16:29:48 +0000

    Generates multiple permutations of TestNG output to see how things look in EmailableReporter.

---
+++ test/src/test/reports/EmailableReportDriver.java
@@ -0,0 +1,38 @@
+package test.reports;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+/**
+ * Generates multiple permutations of TestNG output to see how things look in EmailableReporter.
+ * 
+ * @author Paul Mendelson
+ * @since 5.3
+ * @version $Revision$
+ */
+@Test
+public class EmailableReportDriver {
+  
+  public void doFailureSansLog() {
+    Assert.fail("show failure in report");
+  }    
+  public void doFailureNested() {
+    Assert.fail("show failure in report",new Exception("Real cuase"));
+  }    
+  public void doFailureWithLog() {
+    Reporter.log("Preparing to fail");
+    Assert.fail("show failure in report");
+  }
+  @Test(expectedExceptions={NumberFormatException.class})
+  public void doExpectedExceptionSansLog() {
+    Reporter.log("step 1");
+    Reporter.log("step 2");
+    Integer.parseInt("BAD TEXT");
+  }
+  @Test(expectedExceptions={NumberFormatException.class})
+  public void doExpectedExceptionWithLog() {
+    Integer.parseInt("BAD TEXT");
+  }
+  
+}
