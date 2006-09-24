07546c895e8e4beb7437ebdd64240eb6c6dc0bbe
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-09-24 16:14:30 +0000

    JDK 1.4 tests for annotation transformers (don't work because the finders are static)

---
+++ test-14/src/test/annotationtransformer/AnnotationTransformerTest.java
@@ -0,0 +1,32 @@
+package test.annotationtransformer;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+
+import test.BaseTest;
+
+public class AnnotationTransformerTest extends BaseTest {
+  
+  /**
+   * @testng.test
+   */
+  public void verifyInvocationCount() {
+    TestNG tng = new TestNG();
+    tng.setTarget(TestNG.JAVADOC_ANNOTATION_TYPE);
+    tng.setSourcePath("test-14/src");
+    tng.setAnnotationTransformer(new MyTransformer());
+    tng.setTestClasses(new Class[] { AnnotationTransformerSampleTest.class});
+    TestListenerAdapter tla = new TestListenerAdapter();
+    tng.addListener(tla);
+    
+    tng.run();
+    
+    List<ITestResult> passed = tla.getPassedTests();
+    Assert.assertEquals(15, passed.size());
+  }
+
+}
