dbd9e1c6a12c6a193e9a45ffa841cda76f7712ec
the.mindstorm <the.mindstorm@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-09-25 22:11:17 +0000

    small fix when passing an IAnnotationTransformer

--- src/main/org/testng/TestNG.java
+++ src/main/org/testng/TestNG.java
@@ -538,9 +538,10 @@ public class TestNG {
   public void addListener(Object listener) {
     if (! (listener instanceof ISuiteListener) 
         && ! (listener instanceof ITestListener)
-        && ! (listener instanceof IReporter))
+        && ! (listener instanceof IReporter)
+        && ! (listener instanceof IAnnotationTransformer))
     {
-      exitWithError("Listener " + listener + " is neither an ITestListener, ISuiteListener nor IReporter");
+      exitWithError("Listener " + listener + " must be one of ITestListener, ISuiteListener, IReporter or IAnnotationTransformer");
     }
     else {
       if (listener instanceof ISuiteListener) {
