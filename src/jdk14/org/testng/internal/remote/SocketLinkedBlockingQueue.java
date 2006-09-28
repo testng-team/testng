8e829480e294a199e773cc1c842c13ae46aa0e4e
cquezel <cquezel@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-09-28 18:35:29 +0000

    Javadoc

--- src/jdk14/org/testng/internal/remote/SocketLinkedBlockingQueue.java
+++ src/jdk14/org/testng/internal/remote/SocketLinkedBlockingQueue.java
@@ -1,14 +1,15 @@
-/*
- * @(#)SocketLinkedBlockingQueue.java
- *
- * Copyright 1999-2004 by Taleo Corporation,
- * 330 St-Vallier East, Suite 400, Quebec city, Quebec, G1K 9C5, CANADA
- * All rights reserved
- */
 package org.testng.internal.remote;
 
 import java.net.Socket;
 
+/**
+ * <code>SocketLinkedBlockingQueue</code> is a wrapper on LinkedBlockingQueue so
+ * we may factor out code common to JDK14 and JDK5+ using different implementation 
+ * of LinkedBlockingQueue
+ *
+ * @author cquezel
+ * @since 5.2
+ */
 public class SocketLinkedBlockingQueue extends edu.emory.mathcs.backport.java.util.concurrent.LinkedBlockingQueue
 {
     public Socket take() throws InterruptedException {
