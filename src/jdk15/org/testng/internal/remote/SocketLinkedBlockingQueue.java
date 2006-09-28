b4bd6de22c00ce051903e59db0a9a9b43a903b49
cquezel <cquezel@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-09-28 17:41:52 +0000

    Container class specific to JDK15

---
+++ src/jdk15/org/testng/internal/remote/SocketLinkedBlockingQueue.java
@@ -0,0 +1,15 @@
+/*
+ * @(#)LinkedBlockingQueue.java
+ *
+ * Copyright 1999-2004 by Taleo Corporation,
+ * 330 St-Vallier East, Suite 400, Quebec city, Quebec, G1K 9C5, CANADA
+ * All rights reserved
+ */
+package org.testng.internal.remote;
+
+import java.net.Socket;
+
+public class SocketLinkedBlockingQueue extends java.util.concurrent.LinkedBlockingQueue<Socket>
+{
+  // wrapper
+}
