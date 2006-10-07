fad421d6d4ca1a438daf74a25dff620db815541c
the.mindstorm <the.mindstorm@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-10-07 22:48:36 +0000

    fixed one TODO from Claude

--- src/main/org/testng/reporters/SuiteHTMLReporter.java
+++ src/main/org/testng/reporters/SuiteHTMLReporter.java
@@ -674,8 +674,7 @@ public class SuiteHTMLReporter implements IReporter {
     // TODO CQ why not iterate over entry set if were going to use the key to get the value?
     for (String propertyFileName : suiteResults.keySet()) {
       
-      // TODO CQ why this cast to SuiteResult instead of ISuiteResult? 
-      SuiteResult sr = (SuiteResult) suiteResults.get(propertyFileName);
+      ISuiteResult sr = suiteResults.get(propertyFileName);
       ITestContext testContext = sr.getTestContext();
       
       StringBuffer sb = new StringBuffer();
