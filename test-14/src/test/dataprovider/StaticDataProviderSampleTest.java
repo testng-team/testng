c28d4e7b7553e8f3f77afa40a585d9921d445453
the.mindstorm <the.mindstorm@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-10-12 07:06:02 +0000

    removed a left-over sysout

--- test-14/src/test/dataprovider/StaticDataProviderSampleTest.java
+++ test-14/src/test/dataprovider/StaticDataProviderSampleTest.java
@@ -8,7 +8,6 @@ public class StaticDataProviderSampleTest {
    * @testng.test dataProvider="static" dataProviderClass="test.dataprovider.StaticProvider"
    */
   public void verifyStatic(String s) {
-    System.out.println("PE AICI");
     Assert.assertEquals(s, "Cedric");
   }
 }
