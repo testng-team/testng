9f260a90db357ece3fb67e574b48009748357ff0
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-10-15 17:15:27 +0000

    Documentation for Annotation Transformers

--- src/main/org/testng/internal/annotations/IAnnotationTransformer.java
+++ src/main/org/testng/internal/annotations/IAnnotationTransformer.java
@@ -5,6 +5,24 @@ import java.lang.reflect.Method;
 
 public interface IAnnotationTransformer {
 
+  /**
+   * This method will be invoked by TestNG to give you a chance
+   * to modify a TestNG annotation read from your test classes.
+   * You can change the values you need by calling any of the
+   * setters on the ITest interface.
+   * 
+   * Note that only one of the three parameters testClass,
+   * testConstructor and testMethod will be non-null.
+   * 
+   * @param annotation The annotation that was read from your
+   * test class.
+   * @param testClass If the annotation was found on a class, this
+   * parameter represents this class (null otherwise).
+   * @param testConstructor If the annotation was found on a constructor,
+   * this parameter represents this constructor (null otherwise).
+   * @param testMethod If the annotation was found on a method,
+   * this parameter represents this method (null otherwise).
+   */
   public void transform(ITest annotation, Class testClass,
       Constructor testConstructor, Method testMethod);
   
