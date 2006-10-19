8de443172bcffb97199873e744fe29336b38302a
the.mindstorm <the.mindstorm@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-10-19 00:52:48 +0000

    fixed formattings

--- src/main/org/testng/internal/thread/IPooledExecutor.java
+++ src/main/org/testng/internal/thread/IPooledExecutor.java
@@ -1,18 +1,19 @@
 package org.testng.internal.thread;
 
+
 import java.util.List;
 
 /**
- * Reduced interface to mimic PooledExecutor.
+ * Reduced interface to mimic <code>ExecutorService</code>.
  *
  * @author <a href='mailto:the_mindstorm@evolva.ro'>Alexandru Popescu</a>
  */
 public interface IPooledExecutor {
-   void execute(Runnable run);
+  void execute(Runnable run);
 
-   void shutdown();
+  void shutdown();
 
-   void awaitTermination(long timeout) throws InterruptedException;
+  void awaitTermination(long timeout) throws InterruptedException;
 
   /**
    * @return
