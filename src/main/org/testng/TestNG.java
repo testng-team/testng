c17168b065bc279bdd6de8ebfa1ce9a8a258f5c8
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2006-11-08 23:43:36 +0000

    Fixed a typing error that was not detected by generics

--- src/main/org/testng/TestNG.java
+++ src/main/org/testng/TestNG.java
@@ -1052,7 +1052,7 @@ public class TestNG {
     setSourcePath((String) cmdLineArgs.get(TestNGCommandLineArgs.SRC_COMMAND_OPT));
     setAnnotations(((AnnotationTypeEnum) cmdLineArgs.get(TestNGCommandLineArgs.ANNOTATIONS_COMMAND_OPT)));
 
-    List<String> testClasses = (List<String>) cmdLineArgs.get(TestNGCommandLineArgs.TESTCLASS_COMMAND_OPT);
+    List<Class> testClasses = (List<Class>) cmdLineArgs.get(TestNGCommandLineArgs.TESTCLASS_COMMAND_OPT);
     if (null != testClasses) {
       Class[] classes = (Class[]) testClasses.toArray(new Class[testClasses.size()]);
       setTestClasses(classes);
