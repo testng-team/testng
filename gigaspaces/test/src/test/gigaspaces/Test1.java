f303154d9bbfa2cd530eec0368eb594c0604a6ea
gkorland <gkorland@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2007-04-27 10:28:19 +0000

    Issue 5: Add test

---
+++ gigaspaces/test/src/test/gigaspaces/Test1.java
@@ -0,0 +1,15 @@
+package test.gigaspaces;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f1() {
+//    ppp("f1");
+  }
+  
+  private void ppp(String s) {
+    System.out.println("[Test1] " + s);
+  }
+}
+
