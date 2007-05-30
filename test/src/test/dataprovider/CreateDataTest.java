844df789662875c02f3eaa7c3d1c7c4e3e670155
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2007-05-30 15:18:11 +0000

    Release 5.6

---
+++ test/src/test/dataprovider/CreateDataTest.java
@@ -0,0 +1,29 @@
+package test.dataprovider;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CreateDataTest {
+  
+  /**
+   * @testng.data-provider name = "create-data"
+   */
+  @DataProvider(name = "create-data")
+  public Object[][] create() {
+         return new Object[][] {
+                 new Object[] { new MyObject() }
+         };
+  }
+
+  /**
+   * @testng.test data-provider = "create-data"
+   */
+  @Test(dataProvider = "create-data")
+  public void testMyTest(MyObject o) {
+   // do something with o
+  }
+
+
+}
+
+class MyObject {}
