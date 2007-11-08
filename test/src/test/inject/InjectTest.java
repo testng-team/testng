e87c0f6293d14c6666af15176446f19c89a94522
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2007-11-08 21:17:03 +0000

    Added a test for ITestContext injection in @Test methods

---
+++ test/src/test/inject/InjectTest.java
@@ -0,0 +1,17 @@
+package test.inject;
+
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.ITestNGMethod;
+import org.testng.annotations.Test;
+
+public class InjectTest {
+
+  @Test
+  public void verifyTextContextInjection(ITestContext tc) {
+    Assert.assertNotNull(tc);
+    ITestNGMethod[] allMethods = tc.getAllTestMethods();
+    Assert.assertEquals(allMethods.length, 1);
+    Assert.assertEquals(allMethods[0].getMethod().getName(), "verifyTestContextInjection");
+  }
+}
