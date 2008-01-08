52f8a87bd8dc42c068ac217d6383856a34ba7239
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2008-01-08 16:52:09 +0000

    Forgot two files.

---
+++ src/jdk15/org/testng/annotations/TestInstance.java
@@ -0,0 +1,19 @@
+package org.testng.annotations;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * If this annotation is used on a parameter of a data provider, that parameter is the instance
+ * of the test method which is going to be fed by this data provider.
+ * 
+ * This annotation is ignored everywhere else.
+ * 
+ * @author cbeust
+ */
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target({PARAMETER})
+public @interface TestInstance {
+}
---
+++ test/src/test/dataprovider/TestInstanceFactory.java
@@ -0,0 +1,15 @@
+package test.dataprovider;
+
+import org.testng.annotations.Factory;
+
+public class TestInstanceFactory {
+  @Factory
+  public Object[] init() {
+    return new Object[] {
+        new TestInstanceTest(1),
+        new TestInstanceTest(2)
+    };
+  }
+  
+
+}
