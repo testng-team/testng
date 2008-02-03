a8a4cf1df52cfd0d0f127a75f06eca00c76a8bb9
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2008-02-03 23:44:26 +0000

    Forgot a few files.

---
+++ src/main/org/testng/IMethodInstance.java
@@ -0,0 +1,13 @@
+package org.testng;
+
+
+/**
+ * This interface captures a test method along with all the instances it should
+ * be run on.
+ */
+public interface IMethodInstance {
+
+  ITestNGMethod getMethod();
+  
+  Object[] getInstances();
+}
