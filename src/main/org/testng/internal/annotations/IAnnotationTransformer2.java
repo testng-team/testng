3b1da4b7c31ed0f016bd095e11536f5a2824067d
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2008-05-27 02:33:33 +0000

    Javadoc and doc for the new transformers.

--- src/main/org/testng/internal/annotations/IAnnotationTransformer2.java
+++ src/main/org/testng/internal/annotations/IAnnotationTransformer2.java
@@ -3,11 +3,40 @@ package org.testng.internal.annotations;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
+/**
+ * Use this interface instead of IAnnotationTransformer if you want to modify any TestNG
+ * annotation besides @Test.
+ */
 public interface IAnnotationTransformer2 extends IAnnotationTransformer {
+  /**
+   * Transform an IConfiguration annotation. 
+   *
+   * Note that only one of the three parameters testClass,
+   * testConstructor and testMethod will be non-null.
+   * 
+   * @param annotation The annotation that was read from your
+   * test class.
+   * @param testClass If the annotation was found on a class, this
+   * parameter represents this class (null otherwise).
+   * @param testConstructor If the annotation was found on a constructor,
+   * this parameter represents this constructor (null otherwise).
+   * @param testMethod If the annotation was found on a method,
+   * this parameter represents this method (null otherwise).
+   */
   public void transform(IConfiguration annotation, Class testClass,
       Constructor testConstructor, Method testMethod);
   
-  public void transform(IDataProvider annotation, Method testMethod);
+  /**
+   * Transform an IDataProvider annotation. 
+   *
+   * @param method The method annotated with the IDataProvider annotation.
+   */
+  public void transform(IDataProvider annotation, Method method);
 
-  public void transform(IFactory annotation, Method testMethod);
+  /**
+   * Transform an IFactory annotation. 
+   *
+   * @param method The method annotated with the IFactory annotation.
+   */
+  public void transform(IFactory annotation, Method method);
 }
