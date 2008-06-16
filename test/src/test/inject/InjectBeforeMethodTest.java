1ea0aef0a27e89bba58fadd11452b1c4ea8e6a7a
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2008-06-16 16:03:22 +0000

    Expanded the test to cover @AfterMethod as well.

--- test/src/test/inject/InjectBeforeMethodTest.java
+++ test/src/test/inject/InjectBeforeMethodTest.java
@@ -1,12 +1,14 @@
 package test.inject;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class InjectBeforeMethodTest {
-  private int m_index = 0;
+  private int m_beforeIndex = 0;
+  private int m_afterIndex = 0;
   private static final Object[][] DATA = { 
       new Object[] { "a" },
       new Object[] { "b" },
@@ -14,13 +16,19 @@ public class InjectBeforeMethodTest {
   
   @BeforeMethod
   public void before(Object[] parameters) {
-      Assert.assertEquals(DATA[m_index], parameters);
-      m_index++;
+    Assert.assertEquals(DATA[m_beforeIndex], parameters);
+    m_beforeIndex++;
   }
   
   @DataProvider
   public Object[][] dp() {
     return DATA;
+  }
+
+  @AfterMethod
+  public void after(Object[] parameters) {
+    Assert.assertEquals(DATA[m_afterIndex], parameters);
+    m_afterIndex++;    
   }
 
   @Test(dataProvider = "dp")
--- test/src/test/tmp/A.java
+++ test/src/test/tmp/A.java
@@ -1,27 +1,14 @@
 package test.tmp;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class A {
+  @Test(groups = "database")
+  public void test1() { }
   
-  @BeforeMethod
-  public void before(Object[] parameters) {
-    System.out.println("BEFORE, PARAM:");// + parameters.length + " " + parameters[0]);
-  }
-  
-  @DataProvider
-  public Object[][] dp() {
-    return new Object[][] { 
-        new Object[] { "a" },
-        new Object[] { "b" },
-    };
-  }
+  @Test
+  public void test2() { }
 
-  @Test(dataProvider = "dp")
-  public void f(String a) {
-    System.out.println("TEST:" + a);
-  }
   
 }
