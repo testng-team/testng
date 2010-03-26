452d91b8d74b5bb31e255042e35077616817d362
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2010-03-26 18:41:04 +0000

    Javadoc and doc for @Listeners.

--- src/org/testng/annotations/Listeners.java
+++ src/org/testng/annotations/Listeners.java
@@ -2,11 +2,24 @@ package org.testng.annotations;
 
 import static java.lang.annotation.ElementType.TYPE;
 
+import org.testng.IAnnotationTransformer;
+import org.testng.IAnnotationTransformer2;
 import org.testng.ITestNGListener;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation lets you define listeners directly on a test class
+ * instead of doing so in your testng.xml.  Any class that implements
+ * the interface {@link org.testng.ITestNGListener} is allowed,
+ * except {@link IAnnotationTransformer} and {@link IAnnotationTransformer2},
+ * which need to be defined in XML since they have to be known before we even
+ * start looking for annotations.
+ *
+ * @author Cedric Beust, Mar 26, 2010
+ *
+ */
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target({TYPE})
 public @interface Listeners {
