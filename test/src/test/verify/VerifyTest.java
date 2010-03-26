7d68bb06ed7e4ff52175b7e883dc824ea8dec5c1
cbeust <cbeust@1a8b0fc8-9519-0410-aeec-afd8fd7729cf>
2010-03-26 18:24:32 +0000

    Added tests for @Listeners.

--- test/src/test/verify/VerifyTest.java
+++ test/src/test/verify/VerifyTest.java
@@ -1,52 +1,30 @@
 package test.verify;
 
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
 import org.testng.annotations.Test;
 
-/**
- * Illustrate the implementation of a @Verify/@Verifier test.
- * 
- * One method should be annotated with @Verifier and then each test method
- * annotationed with @Verify will be followed with a call to the @Verifier
- * method.
- */
-@Listeners(VerifyMethodInterceptor.class)
-public class VerifyTest {
+import test.SimpleBaseTest;
 
-  private int m_invocationCount;
+public class VerifyTest extends SimpleBaseTest {
 
-  @BeforeClass
-  public void init() {
-    m_invocationCount = 0;
-  }
-
-  @Verify
-  @Test
-  public void f1() {
-    log("f1");  
-  }
-
-  @Verify
-  @Test
-  public void f2() {
-    log("f2");  
-  }
-
-  @Verifier
-  @Test
-  public void verify() {
-    log("Verifying");
+  private void runTest(Class<?> cls, int expected) {
+    TestNG tng = create(cls);
+    TestListenerAdapter tla = new TestListenerAdapter();
+    tng.addListener(tla);
+    tng.run();
+    
+    Assert.assertEquals(tla.getPassedTests().size(), expected);
   }
 
   @Test
-  public void realVerify() {
-    Assert.assertEquals(m_invocationCount, 4);
+  public void verifyWithAnnotation() {
+    runTest(VerifySampleTest.class, 4);
   }
 
-  private void log(String string) {
-    m_invocationCount++;
-    System.out.println(string);
+  @Test
+  public void verifyWithoutAnnotation() {
+    runTest(VerifyNoListenersSampleTest.class, 3);
   }
 }
