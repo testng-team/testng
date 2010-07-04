9dea444ff3bd764c2e35b165a725bdef4647ba47
Cedric Beust <cedric@beust.com>
2010-07-04 11:33:25 -0700

    Tests for <test preserve-order='true'

---
+++ test/src/test/preserveorder/AAA.java
@@ -0,0 +1,13 @@
+package test.preserveorder;
+
+import org.testng.annotations.Test;
+
+import test.BaseLogTest;
+
+public class AAA extends BaseLogTest {
+
+  @Test
+  public void a() {
+    log("A");
+  }
+}
---
+++ test/src/test/preserveorder/C.java
@@ -0,0 +1,13 @@
+package test.preserveorder;
+
+import org.testng.annotations.Test;
+
+import test.BaseLogTest;
+
+public class C extends BaseLogTest {
+
+  @Test
+  public void c() {
+    log("C");
+  }
+}
