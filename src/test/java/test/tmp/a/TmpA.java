551b3ca332008f64e783f7018858b66a634b8921
Cedric Beust <cedric@beust.com>
2010-10-25 09:19:06 -0700

    Temp files.

--- src/test/java/test/tmp/a/TmpA.java
+++ src/test/java/test/tmp/a/TmpA.java
@@ -3,8 +3,13 @@ package test.tmp.a;
 import org.testng.annotations.Test;
 
 public class TmpA {
-  @Test
+  @Test(groups = "group-a")
   public void tmpa1() {
     System.out.println("tmpa1");
+  }
+
+  @Test
+  public void tmpa2() {
+    System.out.println("tmpa2");
   }
 }
