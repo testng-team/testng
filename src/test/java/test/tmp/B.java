30c4718881e497a3d2ea2cd514410fa2de693aa7
Cedric Beust <cedric@beust.com>
2010-10-25 09:19:18 -0700

    Temp files.

--- src/test/java/test/tmp/B.java
+++ src/test/java/test/tmp/B.java
@@ -4,6 +4,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class B extends C {
+  public static final String G = "group";
+
 
   @BeforeMethod
   public void bm() {
