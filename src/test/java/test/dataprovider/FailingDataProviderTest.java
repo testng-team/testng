41a69f846dc2a14896452b5d74f31c4b93972924
Cedric Beust <cedric@beust.com>
2010-12-20 12:53:10 -0800

    Test refactoring.

--- src/test/java/test/dataprovider/FailingDataProviderTest.java
+++ src/test/java/test/dataprovider/FailingDataProviderTest.java
@@ -6,24 +6,24 @@ import org.testng.TestNG;
 import org.testng.TestNGException;
 import org.testng.annotations.Test;
 
+import test.SimpleBaseTest;
 
-public class FailingDataProviderTest {
-  @Test(description = "TESTNG-142: Exceptions in DataProvider are not reported as failed test")
-  public void failingDataProvider() {
-    TestNG testng= new TestNG(false);
-    testng.setTestClasses(new Class[] {FailingDataProvider.class});
+public class FailingDataProviderTest extends SimpleBaseTest {
+  private void shouldSkipOne(Class cls, String message) {
+    TestNG testng = create(cls);
     TestListenerAdapter tla = new TestListenerAdapter();
     testng.addListener(tla);
-    testng.setVerbose(0);
     testng.run();
-    Assert.assertEquals(tla.getSkippedTests().size(), 1, "Test method should be marked as skipped");
+    Assert.assertEquals(tla.getSkippedTests().size(), 1, message);
   }
 
-  @Test(expectedExceptions = TestNGException.class,
-      description = "TESTNG-447: abort when two data providers have the same name")
+  @Test(description = "TESTNG-142: Exceptions in DataProvider are not reported as failed test")
+  public void failingDataProvider() {
+    shouldSkipOne(FailingDataProvider.class, "Test method should be marked as skipped");
+  }
+
+  @Test(description = "TESTNG-447: Abort when two data providers have the same name")
   public void duplicateDataProviders() {
-    TestNG testng= new TestNG(false);
-    testng.setTestClasses(new Class[] {DuplicateDataProviderSampleTest.class});
-    testng.run();
+    shouldSkipOne(DuplicateDataProviderSampleTest.class, "");
   }
 }
