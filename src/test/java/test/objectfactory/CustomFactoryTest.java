986b64d713372a8a481304379fad50eb3e63dbfa
Cedric Beust <cedric@beust.com>
2010-12-21 10:54:51 -0800

    Reformatting.

--- src/test/java/test/objectfactory/CustomFactoryTest.java
+++ src/test/java/test/objectfactory/CustomFactoryTest.java
@@ -9,20 +9,18 @@ import org.testng.xml.XmlSuite;
 import test.TestHelper;
 
 /**
- * @author Hani Suleiman
- *         Date: Mar 6, 2007
- *         Time: 3:52:19 PM
+ * @author Hani Suleiman Date: Mar 6, 2007 Time: 3:52:19 PM
  */
-public class CustomFactoryTest
-{
+public class CustomFactoryTest {
   @Test
   public void setFactoryOnTestNG() {
     XmlSuite suite = TestHelper.createSuite("test.objectfactory.Simple", "objectfactory");
-    //suite.setObjectFactory(new LoggingObjectFactory());
+    // suite.setObjectFactory(new LoggingObjectFactory());
     TestNG tng = TestHelper.createTestNG(suite);
     tng.setObjectFactory(LoggingObjectFactory.class);
     tng.run();
-    assert LoggingObjectFactory.invoked == 1 : "Logging factory invoked " + LoggingObjectFactory.invoked + " times";
+    assert LoggingObjectFactory.invoked == 1 : "Logging factory invoked "
+        + LoggingObjectFactory.invoked + " times";
   }
 
   @AfterMethod
@@ -36,22 +34,26 @@ public class CustomFactoryTest
     suite.setObjectFactory(new LoggingObjectFactory());
     TestNG tng = TestHelper.createTestNG(suite);
     tng.run();
-    assert LoggingObjectFactory.invoked == 1 : "Logging factory invoked " + LoggingObjectFactory.invoked + " times";
+    assert LoggingObjectFactory.invoked == 1 : "Logging factory invoked "
+        + LoggingObjectFactory.invoked + " times";
   }
 
   @Test(enabled = false, description = "This broke after I made the change to enable AbstractTest")
   public void setFactoryByAnnotation() {
     XmlSuite suite = TestHelper.createSuite("test.objectfactory.Simple", "objectfactory");
-    suite.getTests().get(0).getXmlClasses().add(new XmlClass("test.objectfactory.MyFactoryFactory"));
+    suite.getTests().get(0).getXmlClasses()
+        .add(new XmlClass("test.objectfactory.MyFactoryFactory"));
     TestNG tng = TestHelper.createTestNG(suite);
     tng.run();
-    assert LoggingObjectFactory.invoked == 1 : "Logging factory invoked " + LoggingObjectFactory.invoked + " times";
+    assert LoggingObjectFactory.invoked == 1 : "Logging factory invoked "
+        + LoggingObjectFactory.invoked + " times";
   }
 
   @Test
   public void factoryReceivesContext() {
     XmlSuite suite = TestHelper.createSuite("test.objectfactory.Simple", "objectfactory");
-    suite.getTests().get(0).getXmlClasses().add(new XmlClass("test.objectfactory.ContextAwareFactoryFactory"));
+    suite.getTests().get(0).getXmlClasses()
+        .add(new XmlClass("test.objectfactory.ContextAwareFactoryFactory"));
     TestNG tng = TestHelper.createTestNG(suite);
     tng.run();
   }
@@ -59,7 +61,8 @@ public class CustomFactoryTest
   @Test(expectedExceptions = TestNGException.class)
   public void setInvalidMethodFactoryByAnnotation() {
     XmlSuite suite = TestHelper.createSuite("test.objectfactory.Simple", "objectfactory");
-    suite.getTests().get(0).getXmlClasses().add(new XmlClass("test.objectfactory.BadMethodFactoryFactory"));
+    suite.getTests().get(0).getXmlClasses()
+        .add(new XmlClass("test.objectfactory.BadMethodFactoryFactory"));
     TestNG tng = TestHelper.createTestNG(suite);
     tng.run();
   }
