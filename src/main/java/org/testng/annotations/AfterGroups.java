31879f4ec0e08aa4f5cb173695547abeae0289fd
Cedric Beust <cedric@beust.com>
2011-04-12 14:54:07 -0700

    Javadoc fix.

--- src/main/java/org/testng/annotations/AfterGroups.java
+++ src/main/java/org/testng/annotations/AfterGroups.java
@@ -7,10 +7,11 @@ import java.lang.annotation.Target;
 @Target(java.lang.annotation.ElementType.METHOD)
 public @interface AfterGroups {
   /**
-   * The list of groups that this configuration method will run before. If specified it overrides the
-   * list of groups provided through {@link #groups()} attribute.
-   * This method is guaranteed to run shortly before the first test method that
-   * belongs to any of these groups is invoked.
+   * The list of groups that this configuration method will run after. If
+   * specified it overrides the list of groups provided through
+   * {@link #groups()} attribute. This method is guaranteed to run shortly
+   * before the first test method that belongs to any of these groups is
+   * invoked.
    */
   public String[] value() default {};
 
