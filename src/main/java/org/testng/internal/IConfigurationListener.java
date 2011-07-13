1545ab464e9119e10600da713ca4370f918f0744
Cedric Beust <cedric@beust.com>
2011-07-13 09:58:21 -0700

    Added Javadocs.

--- src/main/java/org/testng/internal/IConfigurationListener.java
+++ src/main/java/org/testng/internal/IConfigurationListener.java
@@ -3,16 +3,22 @@ package org.testng.internal;
 import org.testng.ITestNGListener;
 import org.testng.ITestResult;
 
-
 /**
- * Listener interface for events related to results of @Configuration method invocations.
- *
- * @author <a href='mailto:the_mindstorm@evolva.ro'>Alexandru Popescu</a>
+ * Listener interface for events related to configuration methods.
  */
 public interface IConfigurationListener extends ITestNGListener {
+  /**
+   * Invoked whenever a configuration method succeeded.
+   */
   void onConfigurationSuccess(ITestResult itr);
 
+  /**
+   * Invoked whenever a configuration method failed.
+   */
   void onConfigurationFailure(ITestResult itr);
 
+  /**
+   * Invoked whenever a configuration method was skipped.
+   */
   void onConfigurationSkip(ITestResult itr);
 }
