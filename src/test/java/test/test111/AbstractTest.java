92f0a924cb048ed0eac3d4911a13e7e7b23f5c90
Cédric Beust <cedric@color.com>
2011-10-18 00:01:53 -0700

    Test clean up.

--- src/test/java/test/test111/AbstractTest.java
+++ src/test/java/test/test111/AbstractTest.java
@@ -2,38 +2,18 @@ package test.test111;
 
 
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public abstract class AbstractTest {
 
 	public static int R=0;
 	
-    @BeforeClass
-    public void beforeClass() {
-//        System.err.println("beforeClass");
-    }
-
-    @BeforeMethod
-    public void beforeMethod() {
-//        System.err.println("beforeMethod");
-    }
-
     @Test
     public void testAbstract() {
-//        System.err.println("Abstract TEST");
-    }
-
-    @AfterMethod
-    public void afterMethod() {
-//        System.err.println("afterMethod");
     }
 
     @AfterClass
     public void afterClass() {
-//        System.err.println("afterClass");
         R++;
     }
 }
--- src/test/java/test/test111/Test1.java
+++ src/test/java/test/test111/Test1.java
@@ -1,13 +1,12 @@
 package test.test111;
 
-import junit.framework.Assert;
-
 import org.testng.annotations.Test;
+
+import junit.framework.Assert;
 
 public class Test1 extends AbstractTest {
     @Test
     public void test() {
-//        System.err.println("TEST");
     	Assert.assertEquals(0, AbstractTest.R);
     }
 }
