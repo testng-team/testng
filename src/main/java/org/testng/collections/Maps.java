9e3605c51ccf60a990153cb8f742d1a6e8b6d894
Cedric Beust <cedric@beust.com>
2012-03-13 10:57:15 -0700

    Added: Reporter display the results in the same order as test methods (Libor Zoubek)

--- src/main/java/org/testng/collections/Maps.java
+++ src/main/java/org/testng/collections/Maps.java
@@ -3,6 +3,7 @@ package org.testng.collections;
 
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Maps {
@@ -21,5 +22,9 @@ public class Maps {
 
   public static <K, V> SetMultiMap<K, V> newSetMultiMap() {
     return new SetMultiMap<K, V>();
+  }
+
+  public static <K, V> Map<K, V> newLinkedHashMap() {
+    return new LinkedHashMap<K, V>();
   }
 }
