b027b0520168ca4ee04a8a68f70b5f78a86ebf9a
Cedric Beust <cedric@beust.com>
2012-06-08 06:48:06 -0700

    New Pair class.

--- src/main/java/org/testng/internal/annotations/JDK15AnnotationFinder.java
+++ src/main/java/org/testng/internal/annotations/JDK15AnnotationFinder.java
@@ -31,6 +31,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.annotations.TestInstance;
 import org.testng.collections.Maps;
+import org.testng.internal.collections.Pair;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
@@ -189,57 +190,6 @@ public class JDK15AnnotationFinder implements IAnnotationFinder {
     }
 
     return result;
-  }
-
-  class Pair<A, B> {
-    public A a;
-    public B b;
-
-    public Pair(A a, B b) {
-      this.a = a;
-      this.b = b;
-    }
-
-    @Override
-    public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((a == null) ? 0 : a.hashCode());
-      result = prime * result + ((b == null) ? 0 : b.hashCode());
-      return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj) {
-        return true;
-      }
-      if (obj == null) {
-        return false;
-      }
-      if (getClass() != obj.getClass()) {
-        return false;
-      }
-      final Pair other = (Pair) obj;
-      if (a == null) {
-        if (other.a != null) {
-          return false;
-        }
-      }
-      else if (!a.equals(other.a)) {
-        return false;
-      }
-      if (b == null) {
-        if (other.b != null) {
-          return false;
-        }
-      }
-      else if (!b.equals(other.b)) {
-        return false;
-      }
-      return true;
-    }
-
-
   }
 
   private void ppp(String string) {
---
+++ src/main/java/org/testng/internal/collections/Pair.java
@@ -0,0 +1,66 @@
+package org.testng.internal.collections;
+
+
+
+
+public class Pair<A, B> {
+  private final A first;
+  private final B second;
+
+  public Pair(A first, B second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  public A first() {
+    return first;
+  }
+
+  public B second() {
+    return second;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((first == null) ? 0 : first.hashCode());
+    result = prime * result + ((second == null) ? 0 : second.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final Pair other = (Pair) obj;
+    if (first == null) {
+      if (other.first != null) {
+        return false;
+      }
+    }
+    else if (!first.equals(other.first)) {
+      return false;
+    }
+    if (second == null) {
+      if (other.second != null) {
+        return false;
+      }
+    }
+    else if (!second.equals(other.second)) {
+      return false;
+    }
+    return true;
+  }
+
+  public static <A, B> Pair<A, B> create(A first, B second) {
+    return new Pair<A, B>(first, second);
+  }
+}
