d225861c9e9dfcac8713c72dd9837cc0f4348663
Cedric Beust <cedric@beust.com>
2012-06-16 13:36:55 -0700

    Test the shadowing of parameters.

--- src/test/java/test/parameters/ShadowTest.java
+++ src/test/java/test/parameters/ShadowTest.java
@@ -22,7 +22,7 @@ public class ShadowTest extends SimpleBaseTest {
     {
       XmlClass c1 = new XmlClass(Shadow1SampleTest.class.getName());
       XmlInclude include1 = new XmlInclude("test1");
-      include1.getParameters().put("a", "First");
+      c1.getParameters().put("a", "First");
       c1.getIncludedMethods().add(include1);
       t.getXmlClasses().add(c1);
     }
@@ -30,7 +30,7 @@ public class ShadowTest extends SimpleBaseTest {
     {
       XmlClass c2 = new XmlClass(Shadow2SampleTest.class.getName());
       XmlInclude include2 = new XmlInclude("test2");
-      include2.getParameters().put("a", "Second");
+      c2.getParameters().put("a", "Second");
       c2.getIncludedMethods().add(include2);
       t.getXmlClasses().add(c2);
     }
