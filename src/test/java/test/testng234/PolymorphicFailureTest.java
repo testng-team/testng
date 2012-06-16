4f9bc2b3f9f95e9f257b6184aefa86629db8c26c
Cedric Beust <cedric@beust.com>
2012-06-16 13:37:22 -0700

    More tests.

---
+++ src/test/java/test/testng234/PolymorphicFailureTest.java
@@ -0,0 +1,28 @@
+package test.testng234;
+
+import org.testng.Assert;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+
+import test.SimpleBaseTest;
+
+import java.util.Arrays;
+
+public class PolymorphicFailureTest extends SimpleBaseTest {
+
+  @Test
+  public void superclassFailureShouldCauseFailure() {
+    TestNG tng = create(ChildTest.class);
+    TestListenerAdapter tla = new TestListenerAdapter();
+    tng.addListener(tla);
+    tng.run();
+
+    assertTestResultsEqual(tla.getSkippedTests(), Arrays.asList(
+        "polymorphicMethod",
+        "executePolymorphicMethod")
+        );
+    Assert.assertEquals(0, tla.getPassedTests().size());
+    Assert.assertEquals(0, tla.getFailedTests().size());
+  }
+}
