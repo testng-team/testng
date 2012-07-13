8b60c7d068721a8aa1e2292309c736a5648ff80b
Cedric Beust <cedric@beust.com>
2012-07-12 23:50:31 -0700

    Version bump.

--- src/main/java/org/testng/internal/Version.java
+++ src/main/java/org/testng/internal/Version.java
@@ -1,7 +1,7 @@
 package org.testng.internal;
 
 public class Version {
-  public static final String VERSION = "6.6beta 20120810_0708";
+  public static final String VERSION = "6.6beta 20120812_2350";
 
   public static void displayBanner() {
     System.out.println("...\n... TestNG " + VERSION + " by CŽdric Beust (cedric@beust.com)\n...\n");
