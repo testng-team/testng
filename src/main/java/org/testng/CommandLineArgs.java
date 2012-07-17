8d66b5b025416a08ee2ef9072dca52ea0995b0b5
Cedric Beust <cedric@beust.com>
2012-07-17 08:11:41 -0700

    Updated JCommander to 1.27.

--- src/main/java/org/testng/CommandLineArgs.java
+++ src/main/java/org/testng/CommandLineArgs.java
@@ -1,7 +1,7 @@
 package org.testng;
 
 import com.beust.jcommander.Parameter;
-import com.beust.jcommander.converters.CommaSeparatedConverter;
+import com.beust.jcommander.converters.CommaParameterSplitter;
 
 import org.testng.collections.Lists;
 
@@ -135,7 +135,7 @@ public class CommandLineArgs {
 
   public static final String METHODS = "-methods";
   @Parameter(names = METHODS, description = "Comma separated of test methods",
-      converter = CommaSeparatedConverter.class)
+      splitter = CommaParameterSplitter.class)
   public List<String> commandLineMethods = new ArrayList<String>();
 
   public static final String SUITE_THREAD_POOL_SIZE = "-suitethreadpoolsize";
