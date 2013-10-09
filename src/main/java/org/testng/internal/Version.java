95153335fb35b0e0babcf56de59b3400e26d9485
Cedric Beust <cedric@beust.com>
2013-10-09 08:25:28 -0700

    Version.

--- src/main/java/org/testng/internal/Version.java
+++ src/main/java/org/testng/internal/Version.java
@@ -1,7 +1,7 @@
 package org.testng.internal;
 
 public class Version {
-  public static final String VERSION = "6.8.2beta_20130330_0839";
+  public static final String VERSION = "6.8.9beta";
 
   public static void displayBanner() {
     System.out.println("...\n... TestNG " + VERSION + " by Cédric Beust (cedric@beust.com)\n...\n");
