b1c39ae22168010e766a35496aaf9620784d6587
Vladislav Rassokhin <vladrassokhin@gmail.com>
2013-11-02 01:50:04 +0400

    #Fixed testng-453 Encoding issue with XMLReporter +Nullable annotations +Possible NPE fix

--- src/main/java/org/testng/internal/Utils.java
+++ src/main/java/org/testng/internal/Utils.java
@@ -101,15 +101,18 @@ public final class Utils {
     return vResult.toArray(new String[vResult.size()]);
   }
 
-  public static void writeUtf8File(String outputDir, String fileName, XMLStringBuffer xsb,
-      String prefix) {
+  public static void writeUtf8File(String outputDir, String fileName, XMLStringBuffer xsb, String prefix) {
     try {
-      FileWriter fw = new FileWriter(new File(outputDir, fileName));
-      if (prefix != null) {
-        fw.append(prefix);
+      final File file = new File(outputDir, fileName);
+      if (!file.exists()) {
+        file.createNewFile();
       }
-      xsb.toWriter(fw);
-      fw.close();
+      final OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+      if (prefix != null) {
+        w.append(prefix);
+      }
+      xsb.toWriter(w);
+      w.close();
     } catch(IOException ex) {
       ex.printStackTrace();
     }
@@ -123,7 +126,7 @@ public final class Utils {
    * @param fileName the filename
    * @param sb the file content
    */
-  public static void writeUtf8File(String outputDir, String fileName, String sb) {
+  public static void writeUtf8File(@Nullable String outputDir, String fileName, String sb) {
     final String outDirPath= outputDir != null ? outputDir : "";
     final File outDir= new File(outDirPath);
     writeFile(outDir, fileName, escapeUnicode(sb), "UTF-8", false /* don't append */);
@@ -137,7 +140,7 @@ public final class Utils {
    * @param fileName the filename
    * @param sb the file content
    */
-  public static void writeFile(String outputDir, String fileName, String sb) {
+  public static void writeFile(@Nullable String outputDir, String fileName, String sb) {
     final String outDirPath= outputDir != null ? outputDir : "";
     final File outDir= new File(outDirPath);
     writeFile(outDir, fileName, sb, null, false /* don't append */);
@@ -150,7 +153,7 @@ public final class Utils {
    * @param fileName file name
    * @param sb string to be appended to file
    */
-  public static void appendToFile(String outputDir, String fileName, String sb) {
+  public static void appendToFile(@Nullable String outputDir, String fileName, String sb) {
      String outDirPath= outputDir != null ? outputDir : "";
      File outDir= new File(outDirPath);
      writeFile(outDir, fileName, sb, null, true /* append */);
@@ -164,8 +167,11 @@ public final class Utils {
    * @param fileName the filename
    * @param sb the file content
    */
-  private static void writeFile(File outDir, String fileName, String sb, String encoding, boolean append) {
+  private static void writeFile(@Nullable File outDir, String fileName, String sb, @Nullable String encoding, boolean append) {
     try {
+      if (outDir == null) {
+        outDir = new File("").getAbsoluteFile();
+      }
       if (!outDir.exists()) {
         outDir.mkdirs();
       }
@@ -188,7 +194,7 @@ public final class Utils {
     }
   }
 
-  private static void writeFile(File outputFile, String sb, String encoding, boolean append) {
+  private static void writeFile(File outputFile, String sb, @Nullable String encoding, boolean append) {
     BufferedWriter fw = null;
     try {
       if (!outputFile.exists()) {
