009cc0b4f96a8a87b17df3f75b93f3da7ef61f55
Julien Herr <julien.herr@alcatel-lucent.com>
2015-06-12 14:03:05 +0200

    Follow current naming convention

--- src/main/java/org/testng/internal/InvokeMethodRunnable.java
+++ src/main/java/org/testng/internal/InvokeMethodRunnable.java
@@ -4,7 +4,6 @@ import org.testng.IHookable;
 import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -16,8 +15,8 @@ public class InvokeMethodRunnable implements Runnable {
   private ITestNGMethod m_method = null;
   private Object m_instance = null;
   private Object[] m_parameters = null;
-  private final IHookable hookable;
-  private final ITestResult testResult;
+  private final IHookable m_hookable;
+  private final ITestResult m_testResult;
 
   public InvokeMethodRunnable(ITestNGMethod thisMethod,
                               Object instance,
@@ -28,8 +27,8 @@ public class InvokeMethodRunnable implements Runnable {
     m_method = thisMethod;
     m_instance = instance;
     m_parameters = parameters;
-    this.hookable = hookable;
-    this.testResult = testResult;
+    m_hookable = hookable;
+    m_testResult = testResult;
   }
 
   @Override
@@ -51,10 +50,11 @@ public class InvokeMethodRunnable implements Runnable {
       RuntimeException t = null;
       try {
         Method m = m_method.getMethod();
-        if (hookable == null) {
+        if (m_hookable == null) {
           MethodInvocationHelper.invokeMethod(m, m_instance, m_parameters);
         } else {
-          MethodInvocationHelper.invokeHookable(m_instance, m_parameters, hookable, m, testResult);
+          MethodInvocationHelper.invokeHookable(m_instance, m_parameters, m_hookable, m,
+                                                m_testResult);
         }
       }
       catch(Throwable e) {
