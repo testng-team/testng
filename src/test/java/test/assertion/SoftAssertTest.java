34a4ef79fc14059d843702198544237afa966854
Julien Herr <julien.herr@alcatel-lucent.com>
2015-06-25 22:33:24 +0200

    Rework after review

--- src/test/java/test/assertion/SoftAssertTest.java
+++ src/test/java/test/assertion/SoftAssertTest.java
@@ -9,32 +9,26 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class SoftAssertTest {
+
   @Test
-  public void testOnSucceedCalled() throws Exception {
-    final Collection<IAssert> succeed = new ArrayList<IAssert>();
+  public void testOnSucceedAndFailureCalled() throws Exception {
+    final Collection<IAssert> succeed = new ArrayList<>();
+    final Collection<IAssert> failures = new ArrayList<>();
     final SoftAssert sa = new SoftAssert() {
       @Override
       public void onAssertSuccess(IAssert assertCommand) {
         succeed.add(assertCommand);
       }
-    };
-    sa.assertTrue(true);
-    sa.assertTrue(false);
-    Assert.assertEquals(succeed.size(), 1, succeed.toString());
-  }
 
-  @Test
-  public void testOnFailureCalled() throws Exception {
-    final Collection<IAssert> failured = new ArrayList<IAssert>();
-    final SoftAssert sa = new SoftAssert() {
       @Override
       public void onAssertFailure(IAssert assertCommand, AssertionError ex) {
-        failured.add(assertCommand);
+        failures.add(assertCommand);
       }
     };
     sa.assertTrue(true);
     sa.assertTrue(false);
-    Assert.assertEquals(failured.size(), 1, failured.toString());
+    Assert.assertEquals(succeed.size(), 1, succeed.toString());
+    Assert.assertEquals(failures.size(), 1, failures.toString());
   }
 
   @Test
