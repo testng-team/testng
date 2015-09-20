08d4f5d7b9a2dff20f893dcb1d006c1d9ba77aed
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2015-09-20 08:15:57 +0530

    Included message for assertTrue
    Failure messages weren’t getting reported in the case of 
    failures in assertTrue. Fixed it by including 
    reference to the failure message.

--- src/main/java/org/testng/asserts/Assertion.java
+++ src/main/java/org/testng/asserts/Assertion.java
@@ -106,7 +106,7 @@ public class Assertion implements IAssertLifecycle {
 
 
   public void assertTrue(final boolean condition, final String message) {
-    doAssert(new SimpleAssert<Boolean>(condition, Boolean.TRUE) {
+    doAssert(new SimpleAssert<Boolean>(condition, Boolean.TRUE, message) {
       @Override
       public void doAssert() {
         org.testng.Assert.assertTrue(condition, message);
