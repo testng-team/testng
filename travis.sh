141d3ae5084a3a2d9c182cab6c366cc89eded6aa
Julien Herr <julien.herr@alcatel-lucent.com>
2015-10-22 22:43:24 +0200

    Add configuration for PR analysis with Sonarqube

---
+++ travis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./gradlew clean test
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  if [ -n "$SONAR_GITHUB_OAUTH" ]; then
+    ./gradlew sonarqube \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+  else
+    echo "No oauth token available"
+  fi
+fi
