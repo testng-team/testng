4defcf7c04494f79249f45a2b569e76fd072f72a
Cedric Beust <cedric@beust.com>
2015-10-24 00:57:02 -0700

    Build fix.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,6 +1,7 @@
 import com.beust.kobalt.*
 import com.beust.kobalt.api.*
 import com.beust.kobalt.api.annotation.Task
+import com.beust.kobalt.internal.TaskResult
 import com.beust.kobalt.internal.test
 import com.beust.kobalt.plugin.java.javaProject
 import com.beust.kobalt.plugin.kotlin.kotlinProject
@@ -10,13 +11,17 @@ import com.beust.kobalt.plugin.publish.jcenter
 
 import java.nio.file.*
 
+val GENERATED_DIR = "src/generated/java"
+
 val testng = javaProject {
     name = "testng"
     group = "org.testng"
     artifactId = name
-    version = "6.9.6-SNAPSHOT"
+    version = "6.9.9-SNAPSHOT"
     directory = homeDir("java/testng")
     buildDirectory = "kobaltBuild"
+
+    sourceDirectories = arrayListOf("src/main/java", GENERATED_DIR)
 
     dependencies {
         compile("org.apache.ant:ant:1.7.0",
@@ -33,12 +38,11 @@ val a = assemble(testng) {
     }
 }
 
-
 @Task(name = "generateVersionFile", description = "Generate the Version.java file", runBefore = arrayOf("compile"))
-fun createVersionFile(project: Project) : com.beust.kobalt.internal.TaskResult {
+fun generateVersionFile(@Suppress("UNUSED_PARAMETER") project: Project) : TaskResult {
     val dirFrom = testng.directory + "/src/main/resources/org/testng/internal/"
-    val dirTo = testng.directory + "/src/generated/java/org/testng/internal/"
-    println("COPYING VERSION FILE")
+    val dirTo = testng.directory + "/$GENERATED_DIR/org/testng/internal/"
+    println("Copying version file to $dirTo")
     Files.copy(Paths.get(dirFrom + "VersionTemplateJava"), Paths.get(dirTo + "Version.java"),
             StandardCopyOption.REPLACE_EXISTING)
     return com.beust.kobalt.internal.TaskResult()
