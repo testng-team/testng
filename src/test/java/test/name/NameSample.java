1e9f9235fbf278040b5121f4c8844af41573a557
Julien Herr <julien.herr@alcatel-lucent.com>
2015-10-30 15:45:50 +0100

    Add tests for the test naming feature

---
+++ src/test/java/test/name/NameSample.java
@@ -0,0 +1,13 @@
+package test.name;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test(testName = "NAME")
+public class NameSample {
+
+  @Test
+  public void test() {
+    Assert.assertTrue(true);
+  }
+}
---
+++ src/test/java/test/name/NameTest.java
@@ -0,0 +1,46 @@
+package test.name;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+
+import test.SimpleBaseTest;
+
+public class NameTest extends SimpleBaseTest {
+
+  @Test
+  public void itest_test() {
+    TestNG tng = create(SimpleITestSample.class);
+    TestListenerAdapter adapter = new TestListenerAdapter();
+    tng.addListener(adapter);
+
+    tng.run();
+
+    Assert.assertTrue(adapter.getFailedTests().isEmpty());
+    Assert.assertTrue(adapter.getSkippedTests().isEmpty());
+    Assert.assertEquals(adapter.getPassedTests().size(), 1);
+    ITestResult result = adapter.getPassedTests().get(0);
+    Assert.assertEquals(result.getMethod().getMethodName(), "test");
+    Assert.assertEquals(result.getName(), "NAME");
+    Assert.assertEquals(result.getTestName(), "NAME");
+  }
+
+  @Test
+  public void testName_test() {
+    TestNG tng = create(NameSample.class);
+    TestListenerAdapter adapter = new TestListenerAdapter();
+    tng.addListener(adapter);
+
+    tng.run();
+
+    Assert.assertTrue(adapter.getFailedTests().isEmpty());
+    Assert.assertTrue(adapter.getSkippedTests().isEmpty());
+    Assert.assertEquals(adapter.getPassedTests().size(), 1);
+    ITestResult result = adapter.getPassedTests().get(0);
+    Assert.assertEquals(result.getMethod().getMethodName(), "test");
+    Assert.assertEquals(result.getName(), "NAME");
+    Assert.assertEquals(result.getTestName(), null); // TODO should it be NAME too?
+  }
+}
