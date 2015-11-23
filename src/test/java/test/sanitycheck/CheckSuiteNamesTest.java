d8327d6cb1a4821c1190e16ecccabce14ea9fd2d
toolsqa <toolsqa@gmail.com>
2015-11-24 01:13:12 +0530

    This closes #889 , XmlSuite in nested directories results in FIleNotFoundException

--- src/test/java/test/sanitycheck/CheckSuiteNamesTest.java
+++ src/test/java/test/sanitycheck/CheckSuiteNamesTest.java
@@ -78,12 +78,12 @@ public class CheckSuiteNamesTest extends SimpleBaseTest {
     Assert.assertEquals(xmlSuite2.getName(), "SanityCheckSuite (0)");
   }
   
-	@Test
-	public void checkXmlSuiteAddition() throws ParserConfigurationException, SAXException, IOException {
-		TestNG tng = create();
-		String testngXmlPath = getPathToResource("sanitycheck/test-s-b.xml");
-		Parser parser = new Parser(testngXmlPath);	
-		tng.setXmlSuites(parser.parseToList());
-		tng.initializeSuitesAndJarFile();		
-	}
+  @Test
+  public void checkXmlSuiteAddition() throws ParserConfigurationException, SAXException, IOException {
+ 	TestNG tng = create();
+	String testngXmlPath = getPathToResource("sanitycheck/test-s-b.xml");
+	Parser parser = new Parser(testngXmlPath);	
+	tng.setXmlSuites(parser.parseToList());
+	tng.initializeSuitesAndJarFile();		
+  }
 }
