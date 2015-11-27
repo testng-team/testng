7dd2efdfccf63687184ebe7291c0c687add007ba
Cedric Beust <cedric@beust.com>
2015-11-26 18:39:57 -0800

    Update Kobalt to 0.296.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,59 +1,52 @@
-import com.beust.kobalt.*
-import com.beust.kobalt.api.*
+
+import com.beust.kobalt.TaskResult
+import com.beust.kobalt.api.Project
 import com.beust.kobalt.api.annotation.Task
-import com.beust.kobalt.internal.TaskResult
-import com.beust.kobalt.internal.test
 import com.beust.kobalt.plugin.java.javaProject
-import com.beust.kobalt.plugin.kotlin.kotlinProject
 import com.beust.kobalt.plugin.packaging.assemble
-import com.beust.kobalt.plugin.kotlin.kotlinCompiler
-import com.beust.kobalt.plugin.publish.jcenter
 import java.io.File
 
-import java.nio.file.*
-
-val GENERATED_DIR = "src/generated/java"
 val VERSION = "6.9.10-SNAPSHOT"
 
-val testng = javaProject {
-    name = "testng"
+val p = javaProject {
+
+    name = "testNG"
     group = "org.testng"
     artifactId = name
     version = VERSION
-    buildDirectory = "kobaltBuild"
 
-    sourceDirectories = arrayListOf("src/main/java", GENERATED_DIR)
+    sourceDirectories {
+        path("src/generated/java")
+    }
 
     dependencies {
-        compile("org.apache.ant:ant:1.7.0",
-                "junit:junit:4.10",
-                "org.beanshell:bsh:2.0b4",
-                "com.google.inject:guice:4.0:no_aop",
-                "com.beust:jcommander:1.48",
-                "org.yaml:snakeyaml:1.15")
+        compile("com.beust:jcommander:1.48")
+        compile("com.google.inject:guice:4.0")
+        compile("junit:junit:4.10")
+        compile("org.apache.ant:ant:1.7.0")
+        compile("org.beanshell:bsh:2.0b4")
+        compile("org.yaml:snakeyaml:1.15")
     }
+
     dependenciesTest {
-        compile("org.testng:testng:6.9.8", "org.assertj:assertj-core:3.2.0")
+        compile("org.assertj:assertj-core:2.0.0")
     }
 
     assemble {
-        mavenJars {
+        jar {
         }
     }
 }
 
-@Task(name = "generateVersionFile", description = "Generate the Version.java file", runBefore = arrayOf("compile"))
-fun generateVersionFile(@Suppress("UNUSED_PARAMETER") project: Project) : TaskResult {
-    val fileFrom = File(testng.directory + "/src/main/resources/org/testng/internal/VersionTemplateJava")
-    val fileTo = File(testng.directory + "/$GENERATED_DIR/org/testng/internal/Version.java")
-    println("Generating version file to $fileTo")
-
-    val text = StringBuilder()
-    fileFrom.forEachLine() { line ->
-        text.append(line.replace("@version@", VERSION)).append("\n")
+@Task(name = "createVersion", runBefore = arrayOf("compile"), runAfter = arrayOf("clean"), description = "")
+fun taskCreateVersion(project: Project) : TaskResult {
+    val path = "org/testng/internal"
+    with(arrayListOf<String>()) {
+        File("src/main/resources/$path/VersionTemplateJava").forEachLine {
+            add(it.replace("@version@", VERSION))
+        }
+        File("src/generated/java/$path/Version.java").writeText(joinToString("\n"))
     }
-    fileTo.writeText(text.toString())
     return TaskResult()
 }
-
 
