07fce9b607f418a1175191611e9fef33fa84edcd
Andrew Barchuk <raindev@icloud.com>
2015-12-03 20:34:43 +0200

    Test mixed arrays equality in any order of args
    Mixed arrays of boxed/raw values should be equal regardless argument
    order

--- src/test/java/test/asserttests/ArrayEqualityAssertTest.java
+++ src/test/java/test/asserttests/ArrayEqualityAssertTest.java
@@ -48,11 +48,15 @@ public class ArrayEqualityAssertTest {
     public void mixedArraysAssertEquals() {
         assertEquals(new int[]{ 42 }, new Integer[] { 42 },
                 "arrays of wrapped values are compared by value in assertEquals");
+        assertEquals(new Integer[]{ 42 }, new int[] { 42 },
+                "arrays of wrapped values are compared by value in assertEquals");
     }
 
     @Test(expectedExceptions = AssertionError.class)
     public void mixedArraysAssertNotEquals() {
         assertNotEquals(new int[]{ 42 }, new Integer[] { 42 },
+                "arrays of wrapped values are compared by value in assertNotEquals");
+        assertNotEquals(new Integer[]{ 42 }, new int[] { 42 },
                 "arrays of wrapped values are compared by value in assertNotEquals");
     }
 
