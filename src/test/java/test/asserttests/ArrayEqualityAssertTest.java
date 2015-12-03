e910d28348488741f1c2a48e0231b29187dc31da
Andrew Barchuk <raindev@icloud.com>
2015-12-03 20:23:50 +0200

    Add assertions test for mixing boxed/raw arrays

--- src/test/java/test/asserttests/ArrayEqualityAssertTest.java
+++ src/test/java/test/asserttests/ArrayEqualityAssertTest.java
@@ -34,13 +34,25 @@ public class ArrayEqualityAssertTest {
 
     @Test
     public void boxedArrayAssertEquals() {
-        assertEquals(new Integer[]{ 42 }, new int[] { 42 },
+        assertEquals(new Integer[]{ 42 }, new Integer[] { 42 },
                 "arrays of wrapped values are compared by value in assertEquals");
     }
 
     @Test(expectedExceptions = AssertionError.class)
     public void boxedArrayAssertNotEquals() {
-        assertNotEquals(new Integer[]{ 42 }, new int[] { 42 },
+        assertNotEquals(new Integer[]{ 42 }, new Integer[] { 42 },
+                "arrays of wrapped values are compared by value in assertNotEquals");
+    }
+
+    @Test
+    public void mixedArraysAssertEquals() {
+        assertEquals(new int[]{ 42 }, new Integer[] { 42 },
+                "arrays of wrapped values are compared by value in assertEquals");
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void mixedArraysAssertNotEquals() {
+        assertNotEquals(new int[]{ 42 }, new Integer[] { 42 },
                 "arrays of wrapped values are compared by value in assertNotEquals");
     }
 
