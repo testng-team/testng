df7efbf7fd8f98e93a09fb4b412b70c611c02288
Julien Herr <julien.herr@alcatel-lucent.com>
2015-12-10 10:09:00 +0100

    Rework PriorityTest

--- src/test/java/test/priority/PriorityTest.java
+++ src/test/java/test/priority/PriorityTest.java
@@ -9,31 +9,34 @@ import test.SimpleBaseTest;
 
 public class PriorityTest extends SimpleBaseTest {
 
-  private void runTest(Class<?> cls, String first, String second, boolean parallel) {
+  private void runTest(Class<?> cls, boolean parallel, String... methods) {
     TestNG tng = create(cls);
-    if (parallel) tng.setParallel(XmlSuite.ParallelMode.METHODS);
+    if (parallel) {
+      tng.setParallel(XmlSuite.ParallelMode.METHODS);
+    }
     tng.run();
-    Assert.assertEquals(BaseSample.m_methods.get(0), first);
-    Assert.assertEquals(BaseSample.m_methods.get(1), second);
+    for (int i=0; i<methods.length; i++) {
+      Assert.assertEquals(BaseSample.m_methods.get(i), methods[i]);
+    }
   }
 
   @Test(enabled = false, description = "Make sure priorities work in parallel mode")
   public void priorityInParallel1() {
-    runTest(WithPrioritySampleTest.class, "first", "second", true /* parallel */);
+    runTest(WithPrioritySampleTest.class, true /* parallel */, "first", "second");
   }
 
   @Test(enabled = false, description = "Make sure priorities work in parallel mode")
   public void priorityInParallel2() {
-    runTest(WithPrioritySample2Test.class, "second", "first", true /* parallel */);
+    runTest(WithPrioritySample2Test.class, true /* parallel */, "second", "first");
   }
 
   @Test(description = "Make sure priorities work in sequential mode")
   public void priorityInSequential1() {
-    runTest(WithPrioritySampleTest.class, "first", "second", false /* sequential */);
+    runTest(WithPrioritySampleTest.class, false /* sequential */, "first", "second");
   }
 
   @Test(description = "Make sure priorities work in sequential mode")
   public void priorityInSequential2() {
-    runTest(WithPrioritySample2Test.class, "second", "first", false /* sequential */);
+    runTest(WithPrioritySample2Test.class, false /* sequential */, "second", "first");
   }
 }
