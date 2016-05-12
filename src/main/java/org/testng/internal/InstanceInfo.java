944aa175201bd683dfdc4a3dc747a17d2505e07f
Julien Herr <julien.herr@al-enterprise.com>
2016-05-12 13:15:12 +0200

    Generify InstanceInfo

--- src/main/java/org/testng/internal/InstanceInfo.java
+++ src/main/java/org/testng/internal/InstanceInfo.java
@@ -2,22 +2,22 @@ package org.testng.internal;
 
 import org.testng.IInstanceInfo;
 
-public class InstanceInfo implements IInstanceInfo {
-  private Class m_instanceClass = null;
-  private Object m_instance = null;
+public class InstanceInfo<T> implements IInstanceInfo<T> {
+  private final Class<T> m_instanceClass;
+  private final T m_instance;
 
-  public InstanceInfo(Class cls, Object instance) {
+  public InstanceInfo(Class<T> cls, T instance) {
     m_instanceClass = cls;
     m_instance = instance;
   }
 
   @Override
-  public Object getInstance() {
+  public T getInstance() {
     return m_instance;
   }
 
   @Override
-  public Class getInstanceClass() {
+  public Class<T> getInstanceClass() {
     return m_instanceClass;
   }
 
