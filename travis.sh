69d8da2caaf6a3bffc43597d832e10ea3bdc663d
Julien Herr <julien.herr@al-enterprise.com>
2016-06-01 16:18:06 +0200

    Run SonarQube analyse on PR

--- travis.sh
+++ travis.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  if [ -n "$SONAR_GITHUB_OAUTH" ]; then
-    ./gradlew sonarqube \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-      -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
-  else
-    echo "No oauth token available"
-  fi
+if [ -n "$SONAR_GITHUB_OAUTH" ]; then
+  ./gradlew sonarqube \
+    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+    -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+    -Dsonar.analysis.mode=issue
+else
+  echo "No oauth token available"
 fi
