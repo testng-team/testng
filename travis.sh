315beb14a1c00536a09d60bf55a08cfd23e0644d
Julien Herr <julien.herr@al-enterprise.com>
2016-06-01 16:37:28 +0200

    Add log on SonarQube analyse on PR from Travis

--- travis.sh
+++ travis.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -ev
 
 if [ -n "$SONAR_GITHUB_OAUTH" ]; then
   ./gradlew sonarqube \
     -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
     -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
-    -Dsonar.analysis.mode=issue
+    -Dsonar.analysis.mode=issue \
+    --stacktrace --info
 else
   echo "No oauth token available"
 fi
