803547853d73b26649a27541358ebf0248e1bd6b
Julien Herr <julien.herr@al-enterprise.com>
2016-06-02 14:46:53 +0200

    Update PR

--- travis.sh
+++ travis.sh
@@ -4,14 +4,15 @@ set -e
 if [ $1 == "sonarqube" ] ; then
   if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ] ; then
     if [ -n "${TRAVIS_PULL_REQUEST}" -a "${TRAVIS_PULL_REQUEST}" != "false" ] ; then
-      if [ -n "${SONAR_GITHUB_OAUTH}" ] ; then
+      if [ -n "${SONAR_GITHUB_OAUTH}" -a -n "${SONAR_TOKEN}" ] ; then
         ./gradlew sonarqube \
           -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
           -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
           -Dsonar.analysis.mode=issues \
+          -Dsonar.login=${SONAR_TOKEN} \
           --stacktrace --info
       else
-        echo "Sonar analyse skipped: No GitHub token available"
+        echo "Sonar analyse skipped: No GitHub or Sonarqube token available"
       fi
     else
       if [ "${TRAVIS_BRANCH}" == "master" ] ; then
