bfd37059a8c7f64a02e6f1b6d21bfffb811a21d1
Cedric Beust <cedric@beust.com>
2016-06-24 02:34:42 -0800

    Build the Groovy tests with Kobalt.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,13 +1,16 @@
 
-import com.beust.kobalt.TaskResult
+import com.beust.kobalt.*
 import com.beust.kobalt.api.Project
 import com.beust.kobalt.api.annotation.Task
 import com.beust.kobalt.plugin.packaging.assemble
-import com.beust.kobalt.project
-import com.beust.kobalt.test
 import java.io.File
 
 val VERSION = "6.9.13-SNAPSHOT"
+
+val r = repos("https://dl.bintray.com/cbeust/maven")
+
+val pl = plugins("com.beust:kobalt-groovy:0.1")
+//        file(homeDir("kotlin/kobalt-groovy/kobaltBuild/libs/kobalt-groovy-0.1.jar")))
 
 val p = project {
     name = "testng"
@@ -16,7 +19,11 @@ val p = project {
     version = VERSION
 
     sourceDirectories {
-        path("src/generated/java")
+        path("src/generated/java", "src/main/groovy")
+    }
+
+    sourceDirectoriesTest {
+        path("src/test/groovy")
     }
 
     dependencies {
@@ -30,7 +37,8 @@ val p = project {
 
     dependenciesTest {
         compile("org.assertj:assertj-core:2.0.0",
-                "org.testng:testng:6.9.9")
+                "org.testng:testng:6.9.9",
+                "org.spockframework:spock-core:1.0-groovy-2.4")
     }
 
     test {
