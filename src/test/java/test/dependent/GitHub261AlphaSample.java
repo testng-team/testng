4bcbae0733e52fc32182a8fd4cc70553cedc9e3d
Julien Herr <julien@herr.fr>
2016-07-14 23:13:19 +0200

    Fix #261 Add non regression test

---
+++ src/test/java/test/dependent/GitHub261AlphaSample.java
@@ -0,0 +1,14 @@
+package test.dependent;
+
+import org.testng.annotations.Test;
+
+public class GitHub261AlphaSample {
+
+    @Test
+    public void testAlpha1() {
+    }
+
+    @Test(dependsOnMethods = "testAlpha1")
+    public void testAlpha2() {
+    }
+}
---
+++ src/test/java/test/dependent/GitHub261BetaSample.java
@@ -0,0 +1,15 @@
+package test.dependent;
+
+import org.testng.annotations.Test;
+
+public class GitHub261BetaSample {
+
+    @Test(dependsOnMethods = "testBeta2")
+    public void testBeta1() {}
+
+    @Test
+    public void testBeta2() {}
+
+    @Test(dependsOnMethods = "testBeta2")
+    public void testBeta3() {}
+}
