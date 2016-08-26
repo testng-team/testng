cf4ccd8a252da407807df8cd342353f163483ab1
Julien Herr <herr@protoelectronique.com>
2016-08-26 23:41:35 +0200

    Rework preserve-order tests

--- src/test/java/test/preserveorder/B.java
+++ src/test/java/test/preserveorder/B.java
@@ -2,22 +2,17 @@ package test.preserveorder;
 
 import org.testng.annotations.Test;
 
-import test.BaseLogTest;
-
-public class B extends BaseLogTest {
+public class B {
 
   @Test
   public void b1() {
-    log("B.b1");
   }
 
   @Test
   public void b2() {
-    log("B.b2");
   }
 
   @Test
   public void b3() {
-    log("B.b3");
   }
 }
--- src/test/java/test/preserveorder/PreserveOrderTest.java
+++ src/test/java/test/preserveorder/PreserveOrderTest.java
@@ -2,122 +2,113 @@ package test.preserveorder;
 
 import org.testng.Assert;
 import org.testng.ITestNGListener;
-import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
 
-import test.BaseLogTest;
 import test.InvokedMethodNameListener;
 import test.SimpleBaseTest;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Iterator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PreserveOrderTest extends SimpleBaseTest {
 
-  @Test
-  public void preserveClassOrder() {
-    String[][] tests = new String[][] {
-      new String[] { "AAA", "B", "C" },
-      new String[] { "AAA", "C", "B" },
-      new String[] { "B", "AAA", "C" },
-      new String[] { "B", "C", "AAA" },
-      new String[] { "C", "B", "AAA" },
-      new String[] { "C", "AAA", "B" },
+  @DataProvider
+  public static Object[][] dpTests() {
+    return new Class<?>[][] {
+      new Class<?>[] { A.class, B.class, C.class },
+      new Class<?>[] { A.class, C.class, B.class },
+      new Class<?>[] { B.class, A.class, C.class },
+      new Class<?>[] { B.class, C.class, A.class },
+      new Class<?>[] { C.class, B.class, A.class },
+      new Class<?>[] { C.class, A.class, B.class }
     };
-    for (String[] t : tests) {
-      TestNG tng = create();
-//      tng.setVerbose(2);
-      XmlSuite s = createXmlSuite("Suite");
-      String[] fullTestNames = new String[t.length];
-      for (int i = 0; i < t.length; i++) {
-        fullTestNames[i] = "test.preserveorder." + t[i];
-      }
-      XmlTest xt = createXmlTest(s, "Test", fullTestNames);
-      xt.setPreserveOrder(true);
-//      System.out.println(s.toXml());
-      tng.setXmlSuites(Arrays.asList(s));
-      tng.run();
+  }
 
-      // 3 methods per class, 3 classes, so the log should contain 9 calls
-      List<String> log = BaseLogTest.getLog();
-      Assert.assertEquals(log.size(), 9);
+  @Test(dataProvider = "dpTests")
+  public void preserveClassOrder(Class<?>[] tests) {
+    TestNG tng = create();
+    XmlSuite suite = createXmlSuite("Suite");
+    XmlTest test = createXmlTest(suite, "Test", tests);
+    test.setPreserveOrder(true);
+    tng.setXmlSuites(Arrays.asList(suite));
 
-      // Every slice of 3 logs should belong to the same class in the same
-      // order as in the specified string:  AAA.a1, AAA.a2, AAA.a3, B.a1, etc...
-      // Since we're only testing class ordering in this test, we only match
-      // against the class name
-      for (int i = 0; i < t.length; i++) {
-        for (int j = 0; j < 3; j++) {
-          Assert.assertTrue(log.get(j + (3 * i)).startsWith(t[i] + "."));
-        }
+    InvokedMethodNameListener listener = new InvokedMethodNameListener();
+    tng.addListener((ITestNGListener) listener);
+
+    tng.run();
+
+    assertThat(listener.getInvokedMethodNames()).hasSize(9);
+    Iterator<String> methods = listener.getInvokedMethodNames().iterator();
+    for (Class<?> testClass : tests) {
+      for (int i = 1; i <= 3; i++) {
+        String methodName = methods.next();
+        Assert.assertEquals(methodName, testClass.getSimpleName().toLowerCase() + i);
       }
     }
   }
 
-  @Test
-  public void preserveMethodOrder() {
-    String[][] methods = new String[][] {
-        new String[] { "a1", "a2", "a3" },
-        new String[] { "a1", "a3", "a2" },
-        new String[] { "a2", "a1", "a3" },
-        new String[] { "a2", "a3", "a1" },
-        new String[] { "a3", "a2", "a1" },
-        new String[] { "a3", "a1", "a2" },
+  @DataProvider
+  public static Object[][] dpMethods() {
+    return new String[][] {
+      new String[] { "a1", "a2", "a3" },
+      new String[] { "a1", "a3", "a2" },
+      new String[] { "a2", "a1", "a3" },
+      new String[] { "a2", "a3", "a1" },
+      new String[] { "a3", "a2", "a1" },
+      new String[] { "a3", "a1", "a2" }
     };
+  }
 
-    for (String[] m : methods) {
-      TestNG tng = create();
-  //      tng.setVerbose(2);
-      XmlSuite s = createXmlSuite("Suite");
-      String testName = "test.preserveorder.AAA";
-      XmlTest xt = createXmlTest(s, "Test", testName);
-      addMethods(xt.getXmlClasses().get(0), m);
-      xt.setPreserveOrder(true);
-  //      System.out.println(s.toXml());
-      tng.setXmlSuites(Arrays.asList(s));
-      tng.run();
+  @Test(dataProvider = "dpMethods")
+  public void preserveMethodOrder(String[] methods) {
+    TestNG tng = create();
+    XmlSuite suite = createXmlSuite("Suite");
+    XmlTest test = createXmlTest(suite, "Test", A.class);
+    addMethods(test.getXmlClasses().get(0), methods);
+    test.setPreserveOrder(true);
+    tng.setXmlSuites(Arrays.asList(suite));
 
-      List<String> log = BaseLogTest.getLog();
-//      System.out.println(log);
-      for (int i = 0; i < log.size(); i++) {
-        if (!log.get(i).endsWith(m[i])) {
-          throw new AssertionError("Expected " + Arrays.asList(m) + " but got " + log);
-        }
-      }
-    }
+    InvokedMethodNameListener listener = new InvokedMethodNameListener();
+    tng.addListener((ITestNGListener) listener);
+
+    tng.run();
+
+    assertThat(listener.getInvokedMethodNames()).containsExactly(methods);
   }
 
   @Test
   public void orderShouldBePreservedWithDependencies() {
     TestNG tng = create();
-    XmlSuite s = createXmlSuite("PreserveOrder");
-    XmlTest t = new XmlTest(s);
-    t.getXmlClasses().add(new XmlClass("test.preserveorder.ChuckTest4"));
-    t.getXmlClasses().add(new XmlClass("test.preserveorder.ChuckTest3"));
-    t.setPreserveOrder(true);
-    tng.setXmlSuites(Arrays.asList(s));
-    TestListenerAdapter tla = new TestListenerAdapter();
-    tng.addListener(tla);
+    XmlSuite suite = createXmlSuite("PreserveOrder");
+    XmlTest test = createXmlTest(suite, "Test", Chuck4Sample.class, Chuck3Sample.class);
+    test.setPreserveOrder(true);
+    tng.setXmlSuites(Arrays.asList(suite));
+
+    InvokedMethodNameListener listener = new InvokedMethodNameListener();
+    tng.addListener((ITestNGListener) listener);
+
     tng.run();
 
-    verifyPassedTests(tla, "c4TestOne", "c4TestTwo", "c4TestThree",
-        "c3TestOne", "c3TestTwo", "c3TestThree");
+    assertThat(listener.getSucceedMethodNames()).containsExactly(
+            "c4TestOne", "c4TestTwo", "c4TestThree",
+            "c3TestOne", "c3TestTwo", "c3TestThree"
+    );
   }
 
   @Test(description = "preserve-order on a factory doesn't cause an NPE")
   public void factoryPreserve() {
     TestNG tng = create();
-    XmlSuite s = createXmlSuite("FactoryPreserve");
-    XmlTest t = new XmlTest(s);
-    t.getXmlClasses().add(new XmlClass(TestClassFactory.class.getName()));
-    t.setPreserveOrder(true);
-    tng.setXmlSuites(Arrays.asList(s));
+    XmlSuite suite = createXmlSuite("FactoryPreserve");
+    XmlTest test = createXmlTest(suite, "Test", ClassSampleFactory.class);
+    test.setPreserveOrder(true);
+    tng.setXmlSuites(Arrays.asList(suite));
+
     tng.run();
   }
 
