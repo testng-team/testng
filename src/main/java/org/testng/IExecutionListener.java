28c1bbd966fb4bb0eeefe786d1f58b47f5cb7599
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2016-11-08 08:27:15 +0530

    Make IExecutionListener implementation be the last reporter call before JVM exits
    Fixes #1231
    
    Through this delivery listener invocation has been 
    streamlined such that IExecutionListener’s implementation
    will be the first and last call invoked by TestNG.
    
    * onExecutionStart - Is the first call by TestNG before
    any other listener is invoked.
    * onExecutionFinish - Is the last call by TestNG (after
    report generation is completed). After this call TestNG
    will exit the JVM.

--- src/main/java/org/testng/IExecutionListener.java
+++ src/main/java/org/testng/IExecutionListener.java
@@ -2,6 +2,14 @@ package org.testng;
 
 /**
  * A listener used to monitor when a TestNG run starts and ends.
+ * When implementation of this listener is wired into TestNG, TestNG will ensure that
+ * <ul>
+ * <li>{@link IExecutionListener#onExecutionStart()} gets invoked before TestNG proceeds with invoking any other
+ * listener.</li>
+ * <li>{@link IExecutionListener#onExecutionFinish()} gets invoked at the very last (after report generation phase),
+ * before TestNG exits the JVM.
+ * </li>
+ * </ul>
  *
  * @author Cedric Beust <cedric@beust.com>
  */
