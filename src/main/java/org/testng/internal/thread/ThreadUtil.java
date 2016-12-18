17443bbb3da46c524b05bcf3cbd3835c07bf3964
placebo <sandeshyapuram@gmail.com>
2016-12-18 22:16:06 +0530

    Update ThreadUtil.java
    remove import ```java.util.concurrent.CountDownLatch```

--- src/main/java/org/testng/internal/thread/ThreadUtil.java
+++ src/main/java/org/testng/internal/thread/ThreadUtil.java
@@ -5,7 +5,6 @@ import org.testng.internal.Utils;
 
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
