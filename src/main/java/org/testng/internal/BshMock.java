77b9bb38880212eefee08f2c604d964b89c0d7a0
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2016-12-31 10:44:48 +0530

    Report missing beanshell classes
    Fixes #1293

--- src/main/java/org/testng/internal/BshMock.java
+++ src/main/java/org/testng/internal/BshMock.java
@@ -1,12 +1,15 @@
 package org.testng.internal;
 
 import org.testng.ITestNGMethod;
+import org.testng.TestNGException;
 
 public class BshMock implements IBsh {
 
   @Override
   public boolean includeMethodFromExpression(String expression, ITestNGMethod tm) {
-    return false;
+    String msg = "Beanshell related classes could not be found in CLASSPATH."
+        + "Please add a dependency to bsh.Interpreter (beanshell jar) to your build file and try again.";
+    throw new TestNGException(msg);
   }
 
 }
