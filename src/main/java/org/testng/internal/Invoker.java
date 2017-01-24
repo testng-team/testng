ef82c7e745604e9a743eefe3563d9b35408fbdb2
lijianhua-sdb <lijianhua@sequoiadb.com>
2017-01-24 15:48:24 +0800

    while set parallel to classes the program may occur the exception

--- src/main/java/org/testng/internal/Invoker.java
+++ src/main/java/org/testng/internal/Invoker.java
@@ -88,7 +88,7 @@ public class Invoker implements IInvoker {
   private static final Predicate<ITestNGMethod, IClass> SAME_CLASS = new SameClassNamePredicate();
 
   private void setClassInvocationFailure(Class<?> clazz, Object instance) {
-    synchronized(this){
+    synchronized(m_classInvocationResults){
        Set<Object> instances = m_classInvocationResults.get( clazz );
        if (instances == null) {
          instances = Sets.newHashSet();
@@ -399,7 +399,7 @@ public class Invoker implements IInvoker {
    * @return true if this class or a parent class failed to initialize.
    */
   private boolean classConfigurationFailed(Class<?> cls) {
-    synchronized(this){
+    synchronized(m_classInvocationResults){
        for (Class<?> c : m_classInvocationResults.keySet()) {
          if (c == cls || c.isAssignableFrom(cls)) {
            return true;
@@ -427,7 +427,7 @@ public class Invoker implements IInvoker {
         if (! m_continueOnFailedConfiguration) {
           result = !classConfigurationFailed(cls);
         } else {
-          synchronized(this){
+          synchronized(m_classInvocationResults){
              result = !m_classInvocationResults.get(cls).contains(instance);
           }
         }
@@ -439,7 +439,7 @@ public class Invoker implements IInvoker {
         result = !m_methodInvocationResults.get(currentTestMethod).contains(getMethodInvocationToken(currentTestMethod, instance));
       }
       else if (! m_continueOnFailedConfiguration) {
-        synchronized(this){
+        synchronized(m_classInvocationResults){
            for(Class<?> clazz: m_classInvocationResults.keySet()) {
 //          if (clazz == cls) {
              if(clazz.isAssignableFrom(cls)) {
