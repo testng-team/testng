209e22ed58ad1b8712054162deae25b8d82ce512
Julien Herr <julien@herr.fr>
2017-02-22 22:31:31 +0100

    Check setAccessible usages

--- src/main/java/org/testng/internal/ObjectFactoryImpl.java
+++ src/main/java/org/testng/internal/ObjectFactoryImpl.java
@@ -10,16 +10,10 @@ import java.lang.reflect.Constructor;
  * Note that if no constructor is found matching the specified parameters,
  * this factory will try to invoke a constructor that takes in a string object
  *
- * @author Hani Suleiman
- *         Date: Mar 6, 2007
- *         Time: 12:00:27 PM
  * @since 5.6
  */
 public class ObjectFactoryImpl implements IObjectFactory {
 
-  /**
-   *
-   */
   private static final long serialVersionUID = -4547389328475540017L;
 
   @Override
@@ -27,14 +21,11 @@ public class ObjectFactoryImpl implements IObjectFactory {
     try {
       constructor.setAccessible(true);
       return constructor.newInstance(params);
-    }
-    catch (IllegalAccessException ex) {
+    } catch (IllegalAccessException | InstantiationException ex) {
       return ClassHelper.tryOtherConstructor(constructor.getDeclaringClass());
-    }
-    catch (InstantiationException ex) {
-      return ClassHelper.tryOtherConstructor(constructor.getDeclaringClass());
-    }
-    catch(Exception ex) {
+    } catch (SecurityException e) {
+      throw new TestNGException(constructor.getName() + " must be public", e);
+    } catch(Exception ex) {
       throw new TestNGException("Cannot instantiate class "
           + (constructor != null
                 ? constructor.getDeclaringClass().getName()
