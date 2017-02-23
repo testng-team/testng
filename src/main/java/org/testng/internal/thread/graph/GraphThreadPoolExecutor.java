7ce8421645167315cd2c0ad4ca207c6a950e25f7
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2017-02-23 09:46:26 +0530

    (parallel=‘classes’) not working when coupled with priority
    Fixes #1336.

--- src/main/java/org/testng/internal/thread/graph/GraphThreadPoolExecutor.java
+++ src/main/java/org/testng/internal/thread/graph/GraphThreadPoolExecutor.java
@@ -4,11 +4,13 @@ import org.testng.TestNGException;
 import org.testng.collections.Lists;
 import org.testng.internal.DynamicGraph;
 import org.testng.internal.DynamicGraph.Status;
+import org.testng.internal.thread.IThreadSynchronizer;
 import org.testng.internal.thread.TestNGThreadFactory;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -22,6 +24,7 @@ public class GraphThreadPoolExecutor<T> extends ThreadPoolExecutor {
   private final DynamicGraph<T> m_graph;
   private final List<Runnable> m_activeRunnables = Collections.synchronizedList(Lists.<Runnable>newArrayList());
   private final IThreadWorkerFactory<T> m_factory;
+  private boolean useLatches;
 
   public GraphThreadPoolExecutor(String name, DynamicGraph<T> graph, IThreadWorkerFactory<T> factory, int corePoolSize,
       int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
@@ -32,6 +35,10 @@ public class GraphThreadPoolExecutor<T> extends ThreadPoolExecutor {
     if (m_graph.getFreeNodes().isEmpty()) {
       throw new TestNGException("The graph of methods contains a cycle:" + graph.getEdges());
     }
+  }
+
+  public void setUseLatches(){
+    useLatches = true;
   }
 
   public void run() {
@@ -46,16 +53,30 @@ public class GraphThreadPoolExecutor<T> extends ThreadPoolExecutor {
    */
   private void runNodes(List<T> freeNodes) {
     List<IWorker<T>> runnables = m_factory.createWorkers(freeNodes);
+    CountDownLatch latch = null;
+    if (useLatches && runnables.size() > 1) {
+      latch = new CountDownLatch(runnables.size());
+    }
+    boolean latchUsed = false;
     for (IWorker<T> r : runnables) {
       m_activeRunnables.add(r);
+      if (latch != null && (r instanceof IThreadSynchronizer)) {
+        ((IThreadSynchronizer)r).setLatch(latch);
+        latchUsed = true;
+      }
       setStatus(r, Status.RUNNING);
       try {
         execute(r);
-//        if (m_threadCount > 1) execute(r);
-//        else r.run();
       }
       catch(Exception ex) {
         ex.printStackTrace();
+      }
+    }
+    if (latch != null && latchUsed) {
+      try {
+        latch.await(10, TimeUnit.SECONDS);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
     }
   }
@@ -82,8 +103,5 @@ public class GraphThreadPoolExecutor<T> extends ThreadPoolExecutor {
         runNodes(freeNodes);
       }
     }
-//    if (m_activeRunnables.isEmpty() && m_index < m_runnables.getSize()) {
-//      runNodes(m_index++);
-//    }
   }
 }
