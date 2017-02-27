93575a8dd0dd2259a55939105c02f621b3a399e4
Cedric Beust <cedric.beust@le.com>
2017-02-27 11:22:49 -0800

    6.10 uploaded to JCenter.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -14,7 +14,7 @@ import org.apache.maven.model.Model
 import org.apache.maven.model.Scm
 import java.io.File
 
-val VERSION = "6.10.1-SNAPSHOT"
+val VERSION = "6.11.1-SNAPSHOT"
 
 val bs = buildScript {
     repos("https://dl.bintray.com/cbeust/maven")
