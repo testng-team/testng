8ce119674aeb2e1135a692a5e0cb87e6db7a93ae
Julien Herr <julien@herr.fr>
2017-03-16 21:36:20 +0100

    Add test cases for #1380

--- src/test/java/test/dependent/DependentTest.java
+++ src/test/java/test/dependent/DependentTest.java
@@ -1,21 +1,23 @@
 package test.dependent;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.testng.Assert;
 import org.testng.ITestNGListener;
 import org.testng.TestNG;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.ExpectedExceptions;
 import org.testng.annotations.Test;
-
 import test.BaseTest;
 import test.InvokedMethodNameListener;
 import test.SimpleBaseTest;
 import test.dependent.github1156.ASample;
 import test.dependent.github1156.BSample;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import test.dependent.github1380.GitHub1380Sample;
+import test.dependent.github1380.GitHub1380Sample2;
+import test.dependent.github1380.GitHub1380Sample3;
+import test.dependent.github1380.GitHub1380Sample4;
 
 public class DependentTest extends BaseTest {
 
@@ -24,8 +26,8 @@ public class DependentTest extends BaseTest {
     addClass(SampleDependent1.class.getName());
     run();
     String[] passed = {};
-    String[] failed = { "fail" };
-    String[] skipped = { "shouldBeSkipped" };
+    String[] failed = {"fail"};
+    String[] skipped = {"shouldBeSkipped"};
     verifyTests("Passed", passed, getPassedTests());
     verifyTests("Failed", failed, getFailedTests());
     verifyTests("Skipped", skipped, getSkippedTests());
@@ -35,7 +37,7 @@ public class DependentTest extends BaseTest {
   public void dependentMethods() {
     addClass(SampleDependentMethods.class.getName());
     run();
-    String[] passed = { "oneA", "oneB", "secondA", "thirdA", "canBeRunAnytime" };
+    String[] passed = {"oneA", "oneB", "secondA", "thirdA", "canBeRunAnytime"};
     String[] failed = {};
     String[] skipped = {};
     verifyTests("Passed", passed, getPassedTests());
@@ -47,20 +49,20 @@ public class DependentTest extends BaseTest {
   public void dependentMethodsWithSkip() {
     addClass(SampleDependentMethods4.class.getName());
     run();
-    String[] passed = { "step1", };
-    String[] failed = { "step2", };
-    String[] skipped = { "step3" };
+    String[] passed = {"step1",};
+    String[] failed = {"step2",};
+    String[] skipped = {"step3"};
     verifyTests("Passed", passed, getPassedTests());
     verifyTests("Failed", failed, getFailedTests());
     verifyTests("Skipped", skipped, getSkippedTests());
   }
 
   @Test
-  @ExpectedExceptions({ org.testng.TestNGException.class })
+  @ExpectedExceptions({org.testng.TestNGException.class})
   public void dependentMethodsWithNonExistentMethod() {
     addClass(SampleDependentMethods5.class.getName());
     run();
-    String[] passed = { "step1", "step2" };
+    String[] passed = {"step1", "step2"};
     String[] failed = {};
     String[] skipped = {};
     verifyTests("Passed", passed, getPassedTests());
@@ -84,9 +86,9 @@ public class DependentTest extends BaseTest {
   public void multipleSkips() {
     addClass(MultipleDependentSampleTest.class.getName());
     run();
-    String[] passed = { "init", };
-    String[] failed = { "fail", };
-    String[] skipped = { "skip1", "skip2" };
+    String[] passed = {"init",};
+    String[] failed = {"fail",};
+    String[] skipped = {"skip1", "skip2"};
     verifyTests("Passed", passed, getPassedTests());
     verifyTests("Failed", failed, getFailedTests());
     verifyTests("Skipped", skipped, getSkippedTests());
@@ -97,11 +99,11 @@ public class DependentTest extends BaseTest {
     addClass(InstanceSkipSampleTest.class.getName());
     run();
     verifyInstanceNames("Passed", getPassedTests(),
-        new String[] { "f#1", "f#3", "g#1", "g#3"});
+        new String[]{"f#1", "f#3", "g#1", "g#3"});
     verifyInstanceNames("Failed", getFailedTests(),
-        new String[] { "f#2" });
+        new String[]{"f#2"});
     verifyInstanceNames("Skipped", getSkippedTests(),
-        new String[] { "g#" });
+        new String[]{"g#"});
   }
 
   @Test
@@ -142,6 +144,28 @@ public class DependentTest extends BaseTest {
     tng.run();
 
     assertThat(listener.getSucceedMethodNames()).containsExactly("testB", "testA");
+  }
+
+  @DataProvider
+  public static Object[][] dp1380() {
+    return new Object[][]{
+        {GitHub1380Sample.class, new String[]{"testMethodA", "testMethodB", "testMethodC"}},
+        {GitHub1380Sample2.class, new String[]{"testMethodC", "testMethodB", "testMethodA"}},
+        {GitHub1380Sample3.class, new String[]{"testMethodA", "testMethodB", "testMethodC"}},
+        {GitHub1380Sample4.class, new String[]{"testMethodB", "testMethodA", "testMethodC"}}
+    };
+  }
+
+  @Test(dataProvider = "dp1380", description = "GITHUB-1380")
+  public void simpleCyclingDependencyShouldWork(Class<?> testClass, String[] runMethods) {
+    TestNG tng = SimpleBaseTest.create(testClass);
+
+    InvokedMethodNameListener listener = new InvokedMethodNameListener();
+    tng.addListener((ITestNGListener) listener);
+
+    tng.run();
+
+    assertThat(listener.getSucceedMethodNames()).containsExactly(runMethods);
   }
 }
 
