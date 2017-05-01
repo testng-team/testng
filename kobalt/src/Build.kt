fe27823dfd522e8223142b087d31e2751f0339ed
Cedric Beust <cedric.beust@le.com>
2017-05-01 11:14:38 -0700

    OSGi with Kobalt.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,12 +1,13 @@
 
-import com.beust.kobalt.*
-import com.beust.kobalt.api.*
+import com.beust.kobalt.TaskResult
 import com.beust.kobalt.api.Project
 import com.beust.kobalt.api.annotation.Task
 import com.beust.kobalt.buildScript
+import com.beust.kobalt.plugin.osgi.*
 import com.beust.kobalt.plugin.java.javaCompiler
 import com.beust.kobalt.plugin.packaging.assemble
-import com.beust.kobalt.plugin.publish.*
+import com.beust.kobalt.plugin.publish.autoGitTag
+import com.beust.kobalt.plugin.publish.bintray
 import com.beust.kobalt.project
 import com.beust.kobalt.test
 import org.apache.maven.model.Developer
@@ -16,13 +17,6 @@ import org.apache.maven.model.Scm
 import java.io.File
 
 val VERSION = "6.11.1-SNAPSHOT"
-
-val bs = buildScript {
-    repos("https://dl.bintray.com/cbeust/maven")
-}
-
-//val pl = plugins("com.beust:kobalt-groovy:0.4")
-//        file(homeDir("kotlin/kobalt-groovy/kobaltBuild/libs/kobalt-groovy-0.1.jar")))
 
 val p = project {
     name = "testng"
@@ -95,6 +89,8 @@ val p = project {
     autoGitTag {
         enabled = true
     }
+
+    osgi{}
 }
 
 @Task(name = "createVersion", reverseDependsOn = arrayOf("compile"), runAfter = arrayOf("clean"), description = "")
