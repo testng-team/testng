46c3b641df89da9a6ea185f7c14cca49eb75c890
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2017-08-21 20:12:11 +0530

    Improve error message when data provider returns a null value
    Closes #1509

---
+++ src/test/java/test/dataprovider/Github1509TestClassSample.java
@@ -0,0 +1,18 @@
+package test.dataprovider;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Github1509TestClassSample {
+    @Test(dataProvider = "dp")
+    public void demo(int i) {
+        Assert.assertTrue(i > 0);
+    }
+
+    @DataProvider(name = "dp")
+    public Object[][] getData() {
+        return null;
+    }
+
+}
