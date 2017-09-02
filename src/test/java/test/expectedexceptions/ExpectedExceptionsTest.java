0b474e5008ab9599a91f45935b4d7297d673b01d
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2017-09-02 21:33:57 +0530

    expectedExceptionsMessageRegExp expected/actual messages are not printed
    Closes #1409

--- src/test/java/test/expectedexceptions/ExpectedExceptionsTest.java
+++ src/test/java/test/expectedexceptions/ExpectedExceptionsTest.java
@@ -1,8 +1,14 @@
 package test.expectedexceptions;
 
+import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.Test;
 
 import test.BaseTest;
+import test.expectedexceptions.github1409.TestClassSample;
+
+import java.util.Collection;
+import java.util.List;
 
 public class ExpectedExceptionsTest extends BaseTest {
 
@@ -20,6 +26,19 @@ public class ExpectedExceptionsTest extends BaseTest {
         new String[] { "shouldPass", "shouldPass2", "shouldPass3", "shouldPass4" },
         new String[] { "shouldFail1", "shouldFail2", "shouldFail3", "shouldFail4" },
         new String[] {});
+  }
+
+  @Test
+  public void expectedExceptionsMessage() {
+    getFailedTests().clear();
+    addClass(TestClassSample.class);
+    run();
+    Collection<List<ITestResult>> failedTests = getFailedTests().values();
+    Assert.assertFalse(failedTests.isEmpty());
+    ITestResult result = failedTests.iterator().next().get(0);
+    String actual = result.getThrowable().getMessage().replaceAll("\\n", "");
+    String expected = "The exception was thrown with the wrong message: expected \"expected\" but got \"actual\"";
+    Assert.assertEquals(actual, expected);
   }
 
 }
