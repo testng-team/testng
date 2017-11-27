374d289ee790bdec532c418f0f341d51b7a640f5
Cedric Beust <cedric@beust.com>
2017-11-26 20:02:05 -0800

    Update SNAPSHOT version for Kobalt.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -15,7 +15,7 @@ import org.apache.maven.model.Model
 import org.apache.maven.model.Scm
 import java.io.File
 
-val VERSION = "6.13"
+val VERSION = "6.14-SNAPSHOT"
 
 val p = project {
     name = "testng"
