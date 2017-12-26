fbc9879d08690a054320bdf5701b4801a6496864
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2017-12-26 18:18:58 +0530

    Consider adding a -n (no execute) option
    Closes #1503
    
    Users can now run their tests in a dry run mode 
    by passing the JVM argument : testng.mode.dryrun=true

---
+++ src/test/java/org/testng/DryRunSample.java
@@ -0,0 +1,31 @@
+package org.testng;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DryRunSample {
+
+    @BeforeSuite
+    public void beforeSuite() {
+        throw new RuntimeException("Error");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        throw new RuntimeException("error");
+    }
+
+    @Test(dataProvider = "dp")
+    public void test1(int i) {
+    }
+
+    @DataProvider(name = "dp")
+    public Object[][] getData() {
+        return new Object[][]{
+                {1},
+                {2}
+        };
+    }
+}
