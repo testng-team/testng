99bb5d138acb5dc53a1a0385f5b2c6749785192a
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2018-02-11 21:13:41 +0530

    @beforeGroups executed multiple times in Parallel mode
    Closes #1694
    
    There are two issues that have been fixed:
    
    1. As per java docs, the “values” attribute for 
    @BeforeGroups and @AfterGroups should have precedence
    when provided and should override the value specified
    via “groups” attribute. But “values” is completely
    ignored.
    2. In Parallel mode, @BeforeGroups gets executed
    multiple times.

---
+++ src/test/java/test/beforegroups/issue1694/ChildClassD.java
@@ -0,0 +1,9 @@
+package test.beforegroups.issue1694;
+
+import org.testng.annotations.Test;
+
+public class ChildClassD extends BaseClassWithBeforeGroups {
+    @Test(groups = "regression")
+    public void testMethodD() {
+    }
+}
---
+++ src/test/java/test/beforegroups/issue1694/ChildClassF.java
@@ -0,0 +1,9 @@
+package test.beforegroups.issue1694;
+
+import org.testng.annotations.Test;
+
+public class ChildClassF extends BaseClassWithBeforeGroups {
+    @Test(groups = "regression")
+    public void testMethodF() {
+    }
+}
