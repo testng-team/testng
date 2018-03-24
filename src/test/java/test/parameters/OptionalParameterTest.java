c99bc36c1ab3283ade04b93a7762a9d3778c73cf
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2018-03-24 21:05:21 +0530

    Support using @Optional without needing @Parameters
    Closes #564

--- src/test/java/test/parameters/OptionalParameterTest.java
+++ src/test/java/test/parameters/OptionalParameterTest.java
@@ -2,10 +2,15 @@ package test.parameters;
 
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class OptionalParameterTest {
 
   @Parameters("optional")
   public OptionalParameterTest(@Optional String optional) {}
+
+  @Test(description = "GITHUB-564")
+  public void testWithParameterOnlyOptionalAnnotation(@Optional String unUsedParameter) {
+  }
 
 }
