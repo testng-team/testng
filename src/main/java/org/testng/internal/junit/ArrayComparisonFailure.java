30991cacb1f7cd934c9000f96821b30cd842601e
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2018-06-24 00:05:49 +0530

    [NO-OP Change]Format code internal/junit pkg

--- src/main/java/org/testng/internal/junit/ArrayComparisonFailure.java
+++ src/main/java/org/testng/internal/junit/ArrayComparisonFailure.java
@@ -3,51 +3,48 @@ package org.testng.internal.junit;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Thrown when two array elements differ
- */
+/** Thrown when two array elements differ */
 public class ArrayComparisonFailure extends AssertionError {
 
-    private final List<Integer> fIndices= new ArrayList<>();
-    private final String fMessage;
-    private final AssertionError fCause;
+  private final List<Integer> fIndices = new ArrayList<>();
+  private final String fMessage;
+  private final AssertionError fCause;
 
-    /**
-     * Construct a new <code>ArrayComparisonFailure</code> with an error text and the array's
-     * dimension that was not equal
-     * @param cause the exception that caused the array's content to fail the assertion test 
-     * @param index the array position of the objects that are not equal.
-     */
-    public ArrayComparisonFailure(String message, AssertionError cause, int index) {
-        fMessage= message;
-        fCause= cause;
-        addDimension(index);
-    }
+  /**
+   * Construct a new <code>ArrayComparisonFailure</code> with an error text and the array's
+   * dimension that was not equal
+   *
+   * @param cause the exception that caused the array's content to fail the assertion test
+   * @param index the array position of the objects that are not equal.
+   */
+  public ArrayComparisonFailure(String message, AssertionError cause, int index) {
+    fMessage = message;
+    fCause = cause;
+    addDimension(index);
+  }
 
-    public void addDimension(int index) {
-        fIndices.add(0, index);
-    }
+  public void addDimension(int index) {
+    fIndices.add(0, index);
+  }
 
-    @Override
-    public String getMessage() {
-        StringBuilder builder= new StringBuilder();
-        if (fMessage != null)
-            builder.append(fMessage);
-        builder.append("arrays first differed at element ");
-        for (int each : fIndices) {
-            builder.append("[");
-            builder.append(each);
-            builder.append("]");
-        }
-        builder.append("; ");
-        builder.append(fCause.getMessage());
-        return builder.toString();
+  @Override
+  public String getMessage() {
+    StringBuilder builder = new StringBuilder();
+    if (fMessage != null) builder.append(fMessage);
+    builder.append("arrays first differed at element ");
+    for (int each : fIndices) {
+      builder.append("[");
+      builder.append(each);
+      builder.append("]");
     }
-    
-    /**
-     * {@inheritDoc}
-     */
-    @Override public String toString() {
-        return getMessage();
-    }
+    builder.append("; ");
+    builder.append(fCause.getMessage());
+    return builder.toString();
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public String toString() {
+    return getMessage();
+  }
 }
