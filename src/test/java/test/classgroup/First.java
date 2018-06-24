084e64d7eaf5e92f4a7d2f0ff32319bd92205afa
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2018-06-24 14:34:21 +0530

    [NO-OP Test]Format code class group pkg

--- src/test/java/test/classgroup/First.java
+++ src/test/java/test/classgroup/First.java
@@ -2,7 +2,7 @@ package test.classgroup;
 
 import org.testng.annotations.Test;
 
-@Test(groups = { "first" })
+@Test(groups = {"first"})
 public class First {
   private static boolean m_first1 = false;
   private static boolean m_first2 = false;
@@ -17,7 +17,7 @@ public class First {
     m_first2 = true;
   }
 
-   static boolean allRun() {
+  static boolean allRun() {
     return m_first1 && m_first2;
   }
 }
--- src/test/java/test/classgroup/Second.java
+++ src/test/java/test/classgroup/Second.java
@@ -2,12 +2,11 @@ package test.classgroup;
 
 import org.testng.annotations.Test;
 
-@Test(dependsOnGroups = { "first" })
+@Test(dependsOnGroups = {"first"})
 public class Second {
 
   @Test
   public void verify() {
     assert First.allRun() : "Methods for class First should have been invoked first.";
   }
-
 }
