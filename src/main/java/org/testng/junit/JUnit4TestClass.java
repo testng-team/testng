144521848b65ddcfe69951cc697b6f40059f6159
iignatyev <igor.ignatyev@oracle.com>
2018-08-07 21:17:58 -0700

    TestNG doesn't use the searchable loaders for JUnit tests

--- src/main/java/org/testng/junit/JUnit4TestClass.java
+++ src/main/java/org/testng/junit/JUnit4TestClass.java
@@ -6,6 +6,14 @@ import org.junit.runner.Description;
 public class JUnit4TestClass extends JUnitTestClass {
 
   public JUnit4TestClass(Description test) {
-    super(test.getTestClass());
+    super(descriptionToClass(test));
+  }
+
+  private static Class<?> descriptionToClass(Description test) {
+    Class<?> result = test.getTestClass();
+    if (result == null) {
+        result = org.testng.internal.ClassHelper.forName(test.getClassName());
+    }
+    return result;
   }
 }
