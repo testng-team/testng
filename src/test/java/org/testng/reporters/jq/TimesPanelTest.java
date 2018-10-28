35c67fd4c532b5adf1a4d261307b7fbb8fbdf8b9
oleg.shaburov <shaburov@touchbit.org>
2018-10-29 00:17:31 +0300

    Fixed review remarks

--- src/test/java/org/testng/reporters/jq/TimesPanelTest.java
+++ src/test/java/org/testng/reporters/jq/TimesPanelTest.java
@@ -4,40 +4,30 @@ import org.testng.ISuite;
 import org.testng.annotations.Test;
 import org.testng.internal.paramhandler.FakeSuite;
 import org.testng.reporters.XMLStringBuffer;
-import org.testng.xml.XmlClass;
-import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
+import test.SimpleBaseTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.AssertJUnit.assertTrue;
 
-/**
- * Created by Oleg Shaburov on 28.10.2018
- * shaburov.o.a@gmail.com
- */
-public class TimesPanelTest {
+public class TimesPanelTest extends SimpleBaseTest {
 
     private static final String GITHUB_1931 = "GITHUB-1931 [NPE] TimesPanel.maxTime(ISuite suite)";
 
     @Test(description = GITHUB_1931)
     public void generateReportTimesPanelContentForSuiteWithoutStartedTests() {
-        XmlSuite xmlSuite = new XmlSuite();
-        xmlSuite.setName("GITHUB_1931");
-        XmlTest xmlTest = new XmlTest(xmlSuite);
-        List<XmlClass> xmlClasses = new ArrayList<>();
-        xmlClasses.add(new XmlClass(Object.class));
-        xmlTest.setXmlClasses(xmlClasses);
+        XmlTest xmlTest = createXmlTest("GITHUB_1931", "NPE", Object.class);
         ISuite iSuite = new FakeSuite(xmlTest);
         List<ISuite> suites = new ArrayList<>();
+        suites.add(iSuite);
         Model model = new Model(suites);
-        XMLStringBuffer buffer = new XMLStringBuffer();
-
         TimesPanel panel = new TimesPanel(model);
+        XMLStringBuffer buffer = new XMLStringBuffer();
         panel.getContent(iSuite, buffer);
-        assertThat(panel.getContent(iSuite, buffer), containsString("Total running time: 0 ms"));
+        assertTrue("TimesPanel contains total running time",
+                panel.getContent(iSuite, buffer).contains("Total running time: 0 ms"));
     }
 
 }
