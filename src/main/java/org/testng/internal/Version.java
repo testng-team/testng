28583270df328428c4f889f3da2f5bef93c02adc
Cedric Beust <cedric@beust.com>
2019-08-17 08:39:47 -0700

    Update Version.java to 7.0.0.

--- src/main/java/org/testng/internal/Version.java
+++ src/main/java/org/testng/internal/Version.java
@@ -2,7 +2,7 @@ package org.testng.internal;
 
 public class Version {
 
-  public static final String VERSION = "DEV-SNAPSHOT";
+  public static final String VERSION = "7.0.0";
 
   public static String getVersionString() {
     return VERSION;
