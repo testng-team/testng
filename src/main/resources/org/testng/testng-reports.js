d3268c3ff2ebea35510a925d7619f567f575fb48
Dale Clarke <a.dale.clarke@gmail.com>
2019-09-16 16:44:19 -0700

    Upgraded jQuery from 1.7.1 to 3.4.1 to resolve a prototype pollution vulnerability.

--- src/main/resources/org/testng/testng-reports.js
+++ src/main/resources/org/testng/testng-reports.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('a.navigator-link').click(function() {
+    $('a.navigator-link').on("click", function() {
         // Extract the panel for this link
         var panel = getPanelName($(this));
 
@@ -14,7 +14,7 @@ $(document).ready(function() {
     installMethodHandlers('skipped');
     installMethodHandlers('passed', true); // hide passed methods by default
 
-    $('a.method').click(function() {
+    $('a.method').on("click", function() {
         showMethod($(this));
         return false;
     });
@@ -22,10 +22,10 @@ $(document).ready(function() {
     // Hide all the panels and display the first one (do this last
     // to make sure the click() will invoke the listeners)
     $('.panel').hide();
-    $('.navigator-link').first().click();
+    $('.navigator-link').first().trigger("click");
 
     // Collapse/expand the suites
-    $('a.collapse-all-link').click(function() {
+    $('a.collapse-all-link').on("click", function() {
         var contents = $('.navigator-suite-content');
         if (contents.css('display') == 'none') {
             contents.show();
@@ -56,7 +56,7 @@ function installMethodHandlers(name, hide) {
         return $(sel);
     }
 
-    $('a.hide-methods.' + name).click(function() {
+    $('a.hide-methods.' + name).on("click", function() {
         var w = getContent($(this));
         w.hide();
         getHideLink($(this), name).hide();
@@ -64,7 +64,7 @@ function installMethodHandlers(name, hide) {
     getMethodPanelClassSel($(this), name).hide();
     });
 
-    $('a.show-methods.' + name).click(function() {
+    $('a.show-methods.' + name).on("click", function() {
         var w = getContent($(this));
         w.show();
         getHideLink($(this), name).show();
@@ -74,9 +74,9 @@ function installMethodHandlers(name, hide) {
     });
 
     if (hide) {
-        $('a.hide-methods.' + name).click();
+        $('a.hide-methods.' + name).trigger("click");
     } else {
-        $('a.show-methods.' + name).click();
+        $('a.show-methods.' + name).trigger("click");
     }
 }
 
