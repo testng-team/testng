c483621913f107232bb71005ba8b7ecac138f7bc
Gullapalli Akhil Sai <52671728+GullapalliAkhil@users.noreply.github.com>
2020-02-15 22:46:42 +0530

    Update testng-reports2.js

--- src/main/resources/org/testng/testng-reports2.js
+++ src/main/resources/org/testng/testng-reports2.js
@@ -10,6 +10,9 @@ window.onload=function (){
 
     }
     else if(cookieValue[1]==='Switch Retro Theme'|| localStorage.getItem('Theme')==='Switch Retro Theme'){
+     else if(cookieValue[1]==='Switch Ultra Theme'|| localStorage.getItem('Theme')==='Switch Ultra Theme'){
+         document.getElementById('button').innerText="Switch Retro Theme";
+         document.getElementById('retro').setAttribute('disabled','false');
 
         document.getElementById('button').innerText="Switch Ultra Theme";
         document.getElementById('retro').removeAttribute('disabled');
@@ -23,7 +26,36 @@ window.onload=function (){
         document.getElementById('retro').setAttribute('disabled','false');
         localStorage.setItem('Theme',select);
     }
+     }
+     else if(cookieValue[1]==='Switch Retro Theme'|| localStorage.getItem('Theme')==='Switch Retro Theme'){
+         document.getElementById('button').innerText="Switch Ultra Theme";
+         document.getElementById('ultra').setAttribute('disabled','false');
+     }
  }
+ document.getElementById('button').onclick= function(){
+    let select = document.getElementById('button').innerText;
+    if(select === 'Switch Retro Theme'){
+        let d = new Date();
+        days = 365;
+        d.setTime(+ d + (days * 86400000)); //24 * 60 * 60 * 1000
+        document.cookie="Theme ="+select+ "; expires=" + d.toGMTString()  + ";";
+         document.getElementById('button').innerText="Switch Ultra Theme";
+         document.getElementById('retro').removeAttribute('disabled');
+         document.getElementById('ultra').setAttribute('disabled','false');
+         localStorage.setItem('Theme',select);
+
+     }
+     else if(select ==='Switch Ultra Theme'){
+        let d = new Date();
+        days = 365;
+        d.setTime(+ d + (days * 86400000)); //24 * 60 * 60 * 1000
+        document.cookie="Theme ="+select+ "; expires=" + d.toGMTString()  + ";";
+         document.getElementById('button').innerText="Switch Retro Theme";
+         document.getElementById('ultra').removeAttribute('disabled');
+         document.getElementById('retro').setAttribute('disabled','false');
+         localStorage.setItem('Theme',select);
+     }
+  }
  //Function to mouse hovering affect.
  document.getElementById('button').onmouseover = function(){
     document.getElementById('button').style.borderRadius="25px";
