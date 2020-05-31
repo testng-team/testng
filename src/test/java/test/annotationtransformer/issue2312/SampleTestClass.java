499b3d316eaed39cffb4ac338713ca856e26d6a2
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2020-05-31 23:13:14 +0530

    Streamline IAnnotationTransformer invocation
    Closes #2312

---
+++ src/test/java/test/annotationtransformer/issue2312/SampleTestClass.java
@@ -0,0 +1,13 @@
+package test.annotationtransformer.issue2312;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTestClass {
+
+  @Test
+  public void testMethod() {
+    Assert.assertEquals(1,1);
+  }
+
+}
