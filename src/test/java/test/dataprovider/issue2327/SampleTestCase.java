c650199fb8e0eea2c0c2686cff4bdf41b93458f5
erickubenka <ek@code-fever.de>
2020-06-22 17:54:31 +0200

    Fixing testcase for #2327

--- src/test/java/test/dataprovider/issue2327/SampleTestCase.java
+++ src/test/java/test/dataprovider/issue2327/SampleTestCase.java
@@ -1,22 +1,17 @@
 package test.dataprovider.issue2327;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-@Listeners(CustomListener.class)
 public class SampleTestCase {
 
   @BeforeMethod(alwaysRun = true)
   public void setup(Method method, Object[] params) {
-    if (Arrays.asList(params).contains("Dataset1")) {
-      throw new RuntimeException("setup fail.");
-    }
+    throw new RuntimeException("setup fail.");
   }
 
   @DataProvider(name = "dp", parallel = true)
