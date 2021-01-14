4617869b93564847e500df99fcffa11a2a6db420
Arham Jain <arham.jain@halodoc.com>
2021-01-14 14:43:06 +0530

    test(unit-test): added unit test for the changes made to fix issue 2445

--- src/test/java/test/reports/FailedReporterTest.java
+++ src/test/java/test/reports/FailedReporterTest.java
@@ -1,22 +1,28 @@
 package test.reports;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.testng.Assert;
 import org.testng.ITestNGListener;
 import org.testng.TestNG;
 import org.testng.annotations.Test;
 import org.testng.reporters.FailedReporter;
 import org.testng.xml.Parser;
+import org.testng.xml.SuiteXmlParser;
 import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
 import org.xml.sax.SAXException;
 import test.SimpleBaseTest;
-
+import test.failedreporter.FailedReporterParametersTest;
+import test.thread.Github1636Sample;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.Collections;
 
 public class FailedReporterTest extends SimpleBaseTest {
 
@@ -48,5 +54,23 @@ public class FailedReporterTest extends SimpleBaseTest {
 
     XmlClass failedClass = failedTest.getClasses().get(0);
     Assert.assertEquals("44", failedClass.getAllParameters().get("p"));
+  }
+
+  @Test(description = "ISSUE-2445")
+  public void testParameterPreservationWithFactory() throws IOException {
+    final SuiteXmlParser parser = new SuiteXmlParser();
+    final String file = "src/test/resources/xml/issue2445.xml";
+    final XmlSuite xmlSuite = parser.parse(file, new FileInputStream(file), true);
+    final TestNG tng = create(xmlSuite);
+
+    final Path temp = Files.createTempDirectory("tmp");
+    tng.setOutputDirectory(temp.toAbsolutePath().toString());
+    tng.addListener(new FailedReporter());
+    tng.run();
+
+    final Collection<XmlSuite> failedSuites =
+            new Parser(temp.resolve(FailedReporter.TESTNG_FAILED_XML).toAbsolutePath().toString()).parse();
+    final XmlSuite failedSuite = failedSuites.iterator().next();
+    Assert.assertEquals("value", failedSuite.getAllParameters().get("key"));
   }
 }
