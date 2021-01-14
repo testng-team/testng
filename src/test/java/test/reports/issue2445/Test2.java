ed7a2005bd0d700d18ab28801e4d3de6b798818b
Arham Jain <arham.jain@halodoc.com>
2021-01-14 17:21:26 +0530

    style(test): removed sys out from added test and replaced with definative assert to simulate a successfull test

--- src/test/java/test/reports/issue2445/Test2.java
+++ src/test/java/test/reports/issue2445/Test2.java
@@ -1,11 +1,12 @@
 package test.reports.issue2445;
 
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class Test2 {
   @Test
   public void test2() {
-    System.out.println("Test2 test method");
+    Assert.assertEquals("Simulate success", "1", "1");
   }
 }
 
