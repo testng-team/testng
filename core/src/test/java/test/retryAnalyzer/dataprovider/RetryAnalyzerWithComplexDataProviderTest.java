6e578ddb84ec97ce62bc3b2e2c2f0b46b906e60b
Anindya Sundar Roy <nitts21@gmail.com>
2021-05-19 11:08:18 +0530

    Extract out conditions to test of retryAnalyzer in separate class to test functionality

--- core/src/test/java/test/retryAnalyzer/dataprovider/RetryAnalyzerWithComplexDataProviderTest.java
+++ core/src/test/java/test/retryAnalyzer/dataprovider/RetryAnalyzerWithComplexDataProviderTest.java
@@ -1,55 +1,22 @@
 package test.retryAnalyzer.dataprovider;
 
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
 import org.testng.annotations.Test;
 import test.SimpleBaseTest;
 
+import static org.testng.Assert.assertEquals;
+
 public class RetryAnalyzerWithComplexDataProviderTest extends SimpleBaseTest {
-    private static int countWithStringArray = 3;
-    private static int countWithObjectAndStringArray = 3;
-    private static int countWithSingleParam = 3;
-    private static int countWithoutDataProvider = 3;
 
-    @DataProvider(name = "getTestData")
-    public Object[][] getTestData() {
-        return new String[][]{
-                new String[]{"abc1", "cdf1"}};
-    }
-
-    @Test(dataProvider = "getTestData", description = "GITHUB - 2544", retryAnalyzer = DataProviderRetryAnalyzer.class)
-    public void testWithStringArray(String... values) {
-        System.out.println("Test Run " + values);
-        Assert.assertTrue(countWithStringArray-- == 0);
-    }
-
-    @DataProvider(name = "getObjectData")
-    public Object[][] getObjectData() {
-        return new Object[][]{
-                new Object[]{false, "abc1", "cdf1"}};
-    }
-
-    @Test(dataProvider = "getObjectData", description = "GITHUB - 2544", retryAnalyzer = DataProviderRetryAnalyzer.class)
-    public void testWithObjectAndStringArray(boolean flag, String... values) {
-        System.out.println("Test Run " + values + " with flag: " + flag);
-        Assert.assertTrue(countWithObjectAndStringArray-- == 0);
-    }
-
-    @DataProvider(name = "getSingleParam", parallel = true)
-    public Object[][] getSingleParam() {
-        return new String[][]{
-                new String[]{"abc"},
-                new String[]{"bcd"}};
-    }
-
-    @Test(dataProvider = "getSingleParam", description = "GITHUB - 2544", retryAnalyzer = DataProviderRetryAnalyzer.class)
-    public void testWithSingleParam(String value) {
-        System.out.println("Test Run " + value);
-        Assert.assertTrue(countWithSingleParam-- <= 0);
-    }
-
-    @Test(description = "GITHUB - 2544", retryAnalyzer = DataProviderRetryAnalyzer.class)
-    public void testWithoutDataProvider() {
-        Assert.assertTrue(countWithoutDataProvider-- == 0);
+    @Test(description = "GITHUB-2148")
+    public void ensureTestsAreRetried() {
+        TestNG testng = create(ComplexDataProviderSample.class);
+        TestListenerAdapter tla = new TestListenerAdapter();
+        testng.addListener(tla);
+        testng.run();
+        assertEquals(tla.getPassedTests().size(), 5);
+        assertEquals(tla.getFailedTests().size(), 1);
+        assertEquals(tla.getSkippedTests().size(), 15);
     }
 }
