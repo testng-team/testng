b997ee6c1ca06e391b8f00e9b407a1a5c1efb86e
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2021-06-03 00:38:32 +0300

    Configure releases to Central

--- testng-core/build.gradle.kts
+++ testng-core/build.gradle.kts
@@ -4,25 +4,6 @@ plugins {
     id("testng.sonarqube")
 }
 
-// TODO: remove
-object This {
-    const val version = "7.5.0-SNAPSHOT"
-    const val artifactId = "testng"
-    const val groupId = "org.testng"
-    const val description = "Testing framework for Java"
-    const val url = "https://testng.org"
-    const val scm = "github.com/cbeust/testng"
-
-    // Should not need to change anything below
-    const val name = "TestNG"
-    const val vendor = name
-}
-
-allprojects {
-    group = This.groupId
-    version = This.version
-}
-
 java {
     // use gradle feature
     // in order to optionally exposed transitive dependency
@@ -72,25 +53,5 @@ tasks.test {
         suites("src/test/resources/testng.xml")
         listeners.add("org.testng.reporters.FailedInformationOnConsoleReporter")
         maxHeapSize = "1500m"
-    }
-}
-
-//
-// Releases:
-// ./gradlew publish (to Sonatype, then go to https://oss.sonatype.org/index.html#stagingRepositories to publish)
-//
-
-publishing {
-    repositories {
-        maven {
-            name = "sonatype"
-            url = if (project.version.toString().contains("SNAPSHOT"))
-                uri("https://oss.sonatype.org/content/repositories/snapshots/") else
-                uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = project.findProperty("sonatypeUser")?.toString() ?: System.getenv("SONATYPE_USER")
-                password = project.findProperty("sonatypePassword")?.toString() ?: System.getenv("SONATYPE_PASSWORD")
-            }
-        }
     }
 }
