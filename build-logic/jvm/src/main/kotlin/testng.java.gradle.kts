5bbc3245534b1af705f190729f069384d2831108
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2021-06-04 22:26:28 +0300

    Enhance build matrix in GitHub Actions
    The matrix selects random values for os, jdk, timezone to keep CI
    duration reasonable.
    
    Note: important combinations (e.g. os=windows, os=linux) are listed
    explicitly, so PR builds are always verified for cross-platform compatibility.

--- build-logic/jvm/src/main/kotlin/testng.java.gradle.kts
+++ build-logic/jvm/src/main/kotlin/testng.java.gradle.kts
@@ -12,3 +12,17 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+tasks.withType<JavaCompile>().configureEach {
+    inputs.property("java.version", System.getProperty("java.version"))
+    inputs.property("java.vendor", System.getProperty("java.vendor"))
+    inputs.property("java.vm.version", System.getProperty("java.vm.version"))
+    inputs.property("java.vm.vendor", System.getProperty("java.vm.vendor"))
+}
+
+tasks.withType<Test>().configureEach {
+    inputs.property("java.version", System.getProperty("java.version"))
+    inputs.property("java.vendor", System.getProperty("java.vendor"))
+    inputs.property("java.vm.version", System.getProperty("java.vm.version"))
+    inputs.property("java.vm.vendor", System.getProperty("java.vm.vendor"))
+}
