756b11f7daa5647b3d8f167ad40dab96a81dcd02
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2021-08-21 11:10:09 +0530

    Handle nested classes during parsing classes
    Closes #2536

---
+++ testng-core/src/test/java/test/annotationtransformer/issue2536/data/normal/NonGroupClass2.java
@@ -0,0 +1,11 @@
+package test.annotationtransformer.issue2536.data.normal;
+
+import org.testng.annotations.Test;
+
+@Test
+public class NonGroupClass2 {
+
+  public void step1() {}
+
+  public void step2() {}
+}
