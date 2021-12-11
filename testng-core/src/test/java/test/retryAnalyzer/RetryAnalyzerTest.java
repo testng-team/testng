3025a3535000272f682450fcdb4ff2677ded68a2
Roman_Morskyi <Roman.Morsky@gmail.com>
2021-12-11 12:57:46 +0200

    move testAfterGroupsInvokedAfterRetriedMethod test into RetryAnalyzerTest

--- testng-core/src/test/java/test/retryAnalyzer/RetryAnalyzerTest.java
+++ testng-core/src/test/java/test/retryAnalyzer/RetryAnalyzerTest.java
@@ -37,6 +37,7 @@ import test.retryAnalyzer.issue1697.SampleTestclass;
 import test.retryAnalyzer.issue1946.RetryAnalyzer;
 import test.retryAnalyzer.issue1946.TestclassSample1;
 import test.retryAnalyzer.issue1946.TestclassSample2;
+import test.retryAnalyzer.issue2684.SampleTestClassWithGroupConfigs;
 
 public class RetryAnalyzerTest extends SimpleBaseTest {
   @Test
@@ -255,6 +256,20 @@ public class RetryAnalyzerTest extends SimpleBaseTest {
     TestNG testng = create(suite);
     testng.run();
     Assert.assertEquals(RetryTestSample.count, 3);
+  }
+
+  @Test(description = "GITHUB-2684")
+  public void testAfterGroupsInvokedAfterRetriedMethod() {
+    XmlSuite xmlSuite = createXmlSuite("2684_suite");
+    createXmlTest(xmlSuite, "2684_test", SampleTestClassWithGroupConfigs.class);
+    createXmlGroups(xmlSuite, "2684_group");
+    TestNG testng = create(xmlSuite);
+    InvokedMethodNameListener listener = new InvokedMethodNameListener();
+    testng.addListener(listener);
+    testng.run();
+
+    assertThat(listener.getInvokedMethodNames())
+        .containsExactly("beforeGroups", "testMethod", "testMethod", "afterGroups");
   }
 
   private ITestResult runAssertions(Set<ITestResult> results, String methodName) {
--- testng-core/src/test/java/test/retryAnalyzer/issue2684/SampleTestClassWithGroupConfigs.java
+++ testng-core/src/test/java/test/retryAnalyzer/issue2684/SampleTestClassWithGroupConfigs.java
@@ -2,30 +2,21 @@ package test.retryAnalyzer.issue2684;
 
 import static org.testng.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.testng.annotations.AfterGroups;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 
 public class SampleTestClassWithGroupConfigs {
 
-  public static final List<String> logs = new ArrayList<>();
+  @BeforeGroups("2684_group")
+  public void beforeGroups() {}
 
-  @BeforeGroups("p1")
-  public void beforeGroups() {
-    logs.add("beforeGroups");
-  }
-
-  @Test(groups = "p1", retryAnalyzer = RerunAnalyzer.class)
+  @Test(groups = "2684_group", retryAnalyzer = RerunAnalyzer.class)
   public void testMethod() {
-    logs.add("testMethod");
     RerunAnalyzer.secondTestRetryCount++;
     assertTrue(RerunAnalyzer.secondTestRetryCount > RerunAnalyzer.maxRetryCount);
   }
 
-  @AfterGroups("p1")
-  public void afterGroups() {
-    logs.add("afterGroups");
-  }
+  @AfterGroups("2684_group")
+  public void afterGroups() {}
 }
