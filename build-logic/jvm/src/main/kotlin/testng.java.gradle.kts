66dceb65ca24d387c2f7b1336982a4d044b68e45
Hendrik Duerkop <hendrik.duerkop@tchibo.de>
2022-04-08 21:26:20 +0200

    reduced target jdk back to 11

--- build-logic/jvm/src/main/kotlin/testng.java.gradle.kts
+++ build-logic/jvm/src/main/kotlin/testng.java.gradle.kts
@@ -9,8 +9,8 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks.withType<JavaCompile>().configureEach {
