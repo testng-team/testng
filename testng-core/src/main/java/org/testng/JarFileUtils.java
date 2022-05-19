5d71e0b8cec806c391d13062aa75de0d4e07d836
Martin Aldrin <martin.aldrin@ericsson.com>
2022-05-19 09:38:05 +0200

    #2761 Exception: ERROR java.nio.file.NoSuchFileException: /tmp/testngXmlPathInJar

--- testng-core/src/main/java/org/testng/JarFileUtils.java
+++ testng-core/src/main/java/org/testng/JarFileUtils.java
@@ -69,6 +69,7 @@ class JarFileUtils {
     try (JarFile jf = new JarFile(jarFile)) {
       Enumeration<JarEntry> entries = jf.entries();
       File file = java.nio.file.Files.createTempDirectory("testngXmlPathInJar-").toFile();
+      file.mkdirs();
       String suitePath = null;
       while (entries.hasMoreElements()) {
         JarEntry je = entries.nextElement();
