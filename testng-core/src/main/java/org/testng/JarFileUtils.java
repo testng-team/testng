890ecd9296936012b91bf220bc2bfd6920d97538
speedythesnail <54776159+speedythesnail@users.noreply.github.com>
2022-11-06 11:01:20 -0500

    Update JarFileUtils.java
    Removed logging

--- testng-core/src/main/java/org/testng/JarFileUtils.java
+++ testng-core/src/main/java/org/testng/JarFileUtils.java
@@ -111,11 +111,7 @@ class JarFileUtils {
     if (f.isDirectory()) {
       for (File c : Objects.requireNonNull(f.listFiles())) delete(c);
     }
-
-    /** * Provide better logging for files temp that fail to be cleaned up. */
-    if (!Files.deleteIfExists(f.toPath())) {
-      Utils.log("TestNG", 2, "Failed to delete non-existent temp file: " + f.getPath());
-    }
+    Files.deleteIfExists(f.toPath());
   }
 
   private boolean matchesXmlPathInJar(JarEntry je) {
