0a323d70ff91450fdcb02335ba83c8c1185c06cc
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2023-03-06 08:39:16 +0530

    Include Thread-count in Cloned XmlTest
    Closes #2866

---
+++ testng-core/src/test/java/org/testng/xml/issue2866/ATestClassSample.java
@@ -0,0 +1,9 @@
+package org.testng.xml.issue2866;
+
+import org.testng.annotations.Test;
+
+public class ATestClassSample {
+
+  @Test
+  public void fictitiousTest() {}
+}
---
+++ testng-core/src/test/java/org/testng/xml/issue2866/BTestClassSample.java
@@ -0,0 +1,9 @@
+package org.testng.xml.issue2866;
+
+import org.testng.annotations.Test;
+
+public class BTestClassSample {
+
+  @Test
+  public void fictitiousTest() {}
+}
