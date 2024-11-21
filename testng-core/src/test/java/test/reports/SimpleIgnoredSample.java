36c0ebd94b0288aa9132d98a1507b0dc8d235f3b
Kanaduchi <andrey.topchiy@reltio.com>
2024-11-21 12:20:43 +0000

    Fix for #3189 - fixed ignored testcases count

---
+++ testng-core/src/test/java/test/reports/SimpleIgnoredSample.java
@@ -0,0 +1,13 @@
+package test.reports;
+
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+public class SimpleIgnoredSample {
+
+  @Test
+  @Ignore
+  public void ignored() {
+
+  }
+}
