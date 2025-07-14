e4b2c0623e431808bf579406dfcf911796a90b42
Bartek Florczak <bflorczak@outlook.com>
2025-07-14 12:48:10 +0200

    Add CacheTest for dataprovider issue 3236
    Introduces a new test class to verify dataprovider behavior with retries and caching disabled. The test ensures unique data per retry and validates invocation count.

---
+++ testng-core/src/test/java/test/dataprovider/issue3236/CacheTest.java
@@ -0,0 +1,41 @@
+package test.dataprovider.issue3236;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CacheTest {
+
+  public static final AtomicInteger invocationCount = new AtomicInteger(0);
+  private String currentUuid;
+
+  @Test(dataProvider = "dp", retryAnalyzer = MyRetry.class)
+  public void testMethod(String uuid) {
+    assertEquals(currentUuid, uuid);
+    if (invocationCount.get() != 2) {
+      throw new RuntimeException("Failed for " + uuid);
+    }
+  }
+
+  @DataProvider(name = "dp", cacheDataForTestRetries = false)
+  public Object[][] getData() {
+    invocationCount.incrementAndGet();
+    currentUuid = UUID.randomUUID().toString();
+    return new Object[][]{{currentUuid}};
+  }
+
+  public static class MyRetry implements IRetryAnalyzer {
+
+    private final AtomicInteger counter = new AtomicInteger(1);
+
+    @Override
+    public boolean retry(ITestResult result) {
+      return counter.getAndIncrement() != 2;
+    }
+  }
+}
