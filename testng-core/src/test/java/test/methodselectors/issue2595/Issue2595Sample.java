cce8136435f667dd38f8955d43b23a34723a3128
kalayciburak <kalayciburak1996@gmail.com>
2026-09-05 12:37:14 +0300

    test(selectors): pin equal-priority insertion order
    Keep the #2595 end-to-end case and add a RunInfo test that two
    selectors at priority 10 are both invoked in insertion order.
    Issue2595Sample.throwError now fails the way the original
    reproducer did.

--- testng-core/src/test/java/test/methodselectors/issue2595/Issue2595Sample.java
+++ testng-core/src/test/java/test/methodselectors/issue2595/Issue2595Sample.java
@@ -5,7 +5,9 @@ import org.testng.annotations.Test;
 public class Issue2595Sample {
 
   @Test
-  public void throwError() {}
+  public void throwError() {
+    throw new AssertionError("I shall fail");
+  }
 
   @Test
   public void doNothing() {}
